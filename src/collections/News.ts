2c2dfc00adba4e907435e653f16dda10abf2ea08
Conrad Seto <conrad@alephsf.com>
2024-09-16 11:32:16 -0700

    feat: Create basic News collection and add collection to config

---
+++ src/collections/News.ts
@@ -0,0 +1,78 @@
+import type { CollectionConfig } from 'payload'
+import { fa } from 'payload/i18n/fa'
+import { getSiblingData } from 'payload/shared'
+
+export const News: CollectionConfig = {
+  slug: 'news',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+    },
+    {
+      name:'slug',
+      type:'text',
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'excerpt',
+      type: 'textarea',
+    },
+    {
+      name: 'publishDate',
+      type: 'date',
+      admin: {
+        date: {
+          pickerAppearance: 'dayAndTime',
+        }
+      },
+      required: true,
+    },
+    {
+      name: 'author',
+      type:'relationship',
+      relationTo: 'users',
+      hasMany: true,
+      minRows: 1,
+      required: true,
+    },
+    {
+      name: 'type',
+      type: 'radio',
+      options: [
+        'Flare Updates',
+        'AMA & Interviews',
+        'Past Events',
+        'Ecosystem',
+        'Research',
+      ],
+      defaultValue: 'Flare Updates',
+    },
+    {
+      name: 'pin',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    {
+      name: 'pin priority',
+      type: 'radio',
+      options: [
+        '0',
+        '1',
+        '2',
+        '3',
+      ],
+      defaultValue: '0',
+      admin: {
+        condition: (data, { user }) => {
+          if (data.pin) {
+            return true
+          } else {
+            return false
+          }
+        },
+      },
+    },
+  ]
+}
