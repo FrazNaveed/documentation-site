92864a9c345431ac3e1f424e9186007d9ec9d97b
Conrad Seto <conrad@alephsf.com>
2024-09-19 14:53:02 -0700

    feat: Update query to accept types

--- src/app/_lib/payload/newsQueries.ts
+++ src/app/_lib/payload/newsQueries.ts
@@ -3,9 +3,18 @@ import config from '@payload-config'
 
 const payload = await getPayloadHMR({ config })
 
-export const getNewsData = async () => {
+const getNewsData = async (...types: ('Flare Updates' | 'AMA & Interviews' | 'Past Events' | 'Ecosystem' | 'Research' | null)[]) => {
+  const whereClause: { [key: string]: any } = {}
+
+  if (types.length > 0) {
+    whereClause.type = {
+      in: types.filter((t) => t !== null),
+    }
+  }
+
   const newsData = await payload.find({
     collection: 'news',
+    where: whereClause,
   })
 
   const news = newsData.docs
@@ -13,17 +22,4 @@ export const getNewsData = async () => {
   return news
 }
 
-export const getNewsDataByType = async (type: ('Flare Updates' | 'AMA & Interviews' | 'Past Events' | 'Ecosystem' | 'Research') | null) => {
-  const newsData = await payload.find({
-    collection: 'news',
-    where: {
-      type: {
-        equals: type,
-      },
-    },
-  })
-
-  const news = newsData.docs
-
-  return news
-}
+export default getNewsData
