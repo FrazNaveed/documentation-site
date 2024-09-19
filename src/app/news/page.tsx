78291d09d8dfe2211b81083c4a2ec51e8e4cb20f
Conrad Seto <conrad@alephsf.com>
2024-09-19 14:54:28 -0700

    chore: Display examples of queries filtering by type(s)

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,19 +1,26 @@
 import { get } from 'http'
-import { getNewsData, getNewsDataByType } from '../_lib/payload/newsQueries'
+import getNewsData from '../_lib/payload/newsQueries'
 
 export default async function Page() {
   const news = await getNewsData()
-  const pastEvents = await getNewsDataByType('Past Events')
+  const pastEvents = await getNewsData('Past Events')
+  const twoTypesOfNews = await getNewsData('Past Events', 'Ecosystem')
 
   return (
     <>
       <h1>News</h1>
+      <h2>Default News query</h2>
       {news.map((item, index) => (
         <p key={index}>Title: {item.title}</p>
         )
       )}
-      <h2>Query News by Type 'Past Events'</h2>
+      <h2>Query News by One Type 'Past Events'</h2>
       {pastEvents && pastEvents.map((item, index) => (
+        <p key={index}>Title: {item.title}</p>
+        )
+      )}
+      <h2>Query News by Two Types 'Past Events' and 'Ecosystem'</h2>
+      {twoTypesOfNews && twoTypesOfNews.map((item, index) => (
         <p key={index}>Title: {item.title}</p>
         )
       )}
