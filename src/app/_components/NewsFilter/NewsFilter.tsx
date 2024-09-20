fd339b385a2092433df5049075e1eb0f32ae98d8
Conrad Seto <conrad@alephsf.com>
2024-09-19 22:22:26 -0700

    feat: Rough out bottom posts section

--- src/app/_components/NewsFilter/NewsFilter.tsx
+++ src/app/_components/NewsFilter/NewsFilter.tsx
@@ -1,19 +1,36 @@
 import getNewsData from '../../_lib/payload/newsQueries'
-import React, { JSX } from 'react'
+import React from 'react'
 import Link from 'next/link'
 
 const types = ['All News', 'Flare Updates', 'AMA & Interviews', 'Past Events', 'Ecosystem', 'Research']
+// const news = await getNewsData()
 
-export default function Links(): JSX.Element {
+export default function Links() {
   return (
     <div>
       <nav>
         {types.map((type, index) => (
           <li key={index}>
-            <Link href={`/news/type/${type}`}>{`Type ${type} Link`}</Link>
+            <Link
+              href='/news/#' //{`/news/type/${type}`}
+              // onClick={() => setActiveLink(type)}
+              // className active if this index link is clicked
+              // className={`${activeLink === type ? 'active' : ''}`}
+            >
+              {`Type ${type} Link ${index}`}
+            </Link>
           </li>
         ))}
       </nav>
+      {/* <div>
+        {
+          news.slice(0,3).map((news, index) => (
+            <div>
+              <p key={index}>{news.title}</p>
+            </div>
+          ))
+        }
+      </div> */}
     </div>
-  )
+  );
 }
