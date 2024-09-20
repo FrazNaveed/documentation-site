46633fa1699ded4673f9757bc075c2e7b79adc35
Conrad Seto <conrad@alephsf.com>
2024-09-20 10:33:43 -0700

    fix: Convert where query to filter for news type name and not id

--- src/app/_lib/payload/newsQueries.ts
+++ src/app/_lib/payload/newsQueries.ts
@@ -4,17 +4,13 @@ import config from '@payload-config'
 const payload = await getPayloadHMR({ config })
 
 const getNewsData = async (...types: ('Flare Updates' | 'AMA & Interviews' | 'Past Events' | 'Ecosystem' | 'Research' | null)[]) => {
-  const whereClause: { [key: string]: any } = {}
-
-  if (types.length > 0) {
-    whereClause.type = {
-      in: types.filter((t) => t !== null),
-    }
-  }
-
   const newsData = await payload.find({
     collection: 'news',
-    where: whereClause,
+    where: {
+      'type.name': { 
+        in: types.filter((type) => type !== null)
+      }
+    }
   })
 
   const news = newsData.docs
