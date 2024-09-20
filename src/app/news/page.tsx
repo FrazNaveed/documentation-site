b9cb56decc9a34a0f310af72c4bf42da3d4984f6
Conrad Seto <conrad@alephsf.com>
2024-09-20 13:39:20 -0700

    chore: Change key to item id

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -10,17 +10,17 @@ export default async function Page() {
       <h1>News</h1>
       <h2>Default News query</h2>
       {news.map((item, index) => (
-        <p key={index}>Title: {item.title}</p>
+        <p key={item.id}>Title: {item.title}</p>
         )
       )}
       <h2>Query News by One Type 'Past Events'</h2>
       {pastEvents?.map((item, index) => (
-        <p key={index}>Title: {item.title}</p>
+        <p key={item.id}>Title: {item.title}</p>
         )
       )}
       <h2>Query News by Two Types 'Past Events' and 'Ecosystem'</h2>
       {twoTypesOfNews?.map((item, index) => (
-        <p key={index}>Title: {item.title}</p>
+        <p key={item.id}>Title: {item.title}</p>
         )
       )}
     </>
