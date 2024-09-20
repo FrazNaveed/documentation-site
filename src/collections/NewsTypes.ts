736555f5db2a3307aef84c8cfe91eeef2e5dee34
Conrad Seto <conrad@alephsf.com>
2024-09-20 08:41:33 -0700

    feat: Create News Types Collection for news types taxonomy

---
+++ src/collections/NewsTypes.ts
@@ -0,0 +1,29 @@
+import type { CollectionConfig } from 'payload'
+
+export const NewsTypes: CollectionConfig = {
+  slug: 'news-types',
+  access: {
+    // for seeding purposes; review for production
+    read: () => true,
+    create: () => true,
+  },
+  labels: {
+    singular: 'News Type',
+    plural: 'News Types',
+  },
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      required: true,
+      unique: true,
+    }
+  ],
+}
