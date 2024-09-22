af65d22b30978be23e025856c371cba13c37f74c
Conrad Seto <conrad@alephsf.com>
2024-09-21 20:58:14 -0700

    feat: Add hooks for active state on navigation

--- src/app/_components/NewsFilter/NewsFilter.tsx
+++ src/app/_components/NewsFilter/NewsFilter.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, {useState, useEffect } from 'react'
 import Link from 'next/link'
 import cx from 'classnames'
 import styles from './NewsFilter.module.scss'
@@ -10,12 +11,27 @@ export type LinksProps = {
 
 export default function Links({
   navLinks, className }: LinksProps) {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [hasClicked, setHasClicked] = useState(false)
+
+  const handleClick = (index: number) => {
+    setActiveIndex(index)
+    setHasClicked(true)
+  }
+
   return (
     <div className={cx(styles.NewsFilter, className)}>
       <div className={cx(styles.wrap)}>
         <nav>
           {navLinks?.map((navLink, index) => (
-            <Link href={`/news/${navLink.link}`} className={cx(styles.filter)}>
+            <Link
+              href={index !== 0 ? `/news/type/${navLink.link}` : `/news/`}
+              className={cx(
+                styles.filter,
+                {[styles.active]: !hasClicked && index === 0 || activeIndex === index},
+              )}
+              onClick={() => handleClick(index)}
+              >
               <li key={navLink.id} className={cx(styles.text)}>
                 {navLink.text}
               </li>
