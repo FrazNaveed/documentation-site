5eaead54793275b1c3a796aa116ffa7c53ecc17c
Conrad Seto <conrad@alephsf.com>
2024-09-25 15:55:20 -0700

    refactor: Use function arguments to reduce repetition

--- scripts/seedNewsTypesTranslations.js
+++ scripts/seedNewsTypesTranslations.js
@@ -6,7 +6,7 @@ import { faker } from '@faker-js/faker'
 // Define the base URL of your Payload CMS
 const API_URL = 'http://localhost:3000/api' // Change this to your Payload CMS API URL
 
-const newsTypesIDsEn = await axios.get(`${API_URL}/news-types`)
+const newsTypesIDs = await axios.get(`${API_URL}/news-types`)
   .then((response) => {
     const newsTypesDocs = response.data.docs
     const mappedNewsTypesProp = newsTypesDocs.map((newsType) => +newsType.id)
@@ -17,48 +17,57 @@ const newsTypesIDsEn = await axios.get(`${API_URL}/news-types`)
   .catch((error) => console.error(error))
 
 // Functions to add translations seed data
-const seedNewsTypesDataForTranslations = async (newsTypes) => {
-  const promises = []
+const seedNewsTypesDataForTranslations = async (newsTypes, locale) => {
+  //  const promises = []
 
   for (let i = 0; i < newsTypes.length; i++) {
     const newsTypeId = newsTypes[i]
     // const newsTypeEs = defaultNewsTypesEs.find((newsType) => newsType.name === 'Ecosistema')
     // const newsTypeDe = defaultNewsTypesDe.find((newsType) => newsType.name === 'Ökosystem')
-    const newsTypeNameEs = faker.lorem.word()
-    const newsTypeNameEsTagged = `ES ${newsTypeNameEs}`
-    const newsTypeSlugEs = faker.lorem.slug()
-    const newsTypeNameSlugEsTagged = `es-slug-${newsTypeSlugEs}`
-    const newsTypeNameDe = faker.lorem.word()
-    const newsTypeNameDeTagged = `DE ${newsTypeNameDe}`
-    const newsTypeSlugDe = faker.lorem.slug()
-    const newsTypeNameSlugDeTagged = `de-slug-${newsTypeSlugDe}`
+    const newsTypeName = faker.lorem.word()
+    const newsTypeNameTagged = `${locale.toUpperCase()} ${newsTypeName}`
+    const newsTypeSlug = faker.lorem.slug()
+    const newsTypeNameSlugTagged = `${locale}-slug-${newsTypeSlug}`
 
-    const restUrlEs = `${API_URL}/news-types/${newsTypeId}?locale=es`
-    const restUrlDe = `${API_URL}/news-types/${newsTypeId}?locale=de`
+    const restUrl = `${API_URL}/news-types/${newsTypeId}?locale=${locale}`
 
-    if (newsTypesIDsEn) {
-      promises.push(
-        axios.patch(restUrlEs, { name: newsTypeNameEsTagged, slug: newsTypeNameSlugEsTagged }, {
-          headers: {
-            'Content-Type': 'application/json',
+    if (newsTypesIDs) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const respsonse = await axios.patch(
+          restUrl,
+          {
+            name: newsTypeNameTagged,
+            slug: newsTypeNameSlugTagged,
           },
-        })
-          .catch((error) => console.error('Error creating news item for es locale:', error.response ? error.response.data : error.message)),
-      )
-
-      promises.push(
-        axios.patch(restUrlDe, { name: newsTypeNameDeTagged, slug: newsTypeNameSlugDeTagged }, {
-          headers: {
-            'Content-Type': 'application/json',
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
           },
-        })
-          .catch((error) => console.error('Error creating news item for de locale:', error.response ? error.response.data : error.message)),
-      )
+        )
+      } catch (error) {
+        console.error(
+          `Error creating news item for ${locale} locale:`,
+          error.response ? error.response.data : error.message,
+        )
+      }
+
+      // promises.push(
+      //   axios.patch(restUrlDe, { name: newsTypeNameDeTagged, slug: newsTypeNameSlugDeTagged }, {
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //   })
+      //     .catch((error) => console.error('Error creating news item for de locale:',
+      //  error.response ? error.response.data : error.message)),
+      // )
     }
   }
 
-  await Promise.all(promises)
+  // await Promise.all(promises)
 }
 
 // Seed translations
-seedNewsTypesDataForTranslations(newsTypesIDsEn)
+seedNewsTypesDataForTranslations(newsTypesIDs, 'es')
+seedNewsTypesDataForTranslations(newsTypesIDs, 'de')
