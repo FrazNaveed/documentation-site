bd8b4e1ff49c7b84d2e4a9b508e32f3c9b03ccc9
Conrad Seto <conrad@alephsf.com>
2024-09-27 16:37:33 -0700

    feat: Add successMessage to hook. To-do: reset form after submission.

--- src/app/_hooks/useSubscriptionForm.ts
+++ src/app/_hooks/useSubscriptionForm.ts
@@ -4,6 +4,7 @@ import { submitToSurveyMonkey } from '../_lib/api'
 
 export default function useSubscriptionForm() {
   const [errorMessage, setErrorMessage ] = useState<string | null>(null)
+  const [successMessage, setSuccessMessage] = useState<string | null>(null)
 
   const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -29,10 +30,16 @@ export default function useSubscriptionForm() {
     } else {
       console.info(emailValue, 'passes basic email format check')
       setErrorMessage(null)
+      setSuccessMessage('signup complete')
       submitToSurveyMonkey(emailValue)
       console.log(emailValue, 'submitted')
+
+      // clean up
+      // emailInput.value = ''
+      // setErrorMessage(null)
+      // setButtonState(false)
     }
   }
 
-  return { handleSubmit, errorMessage }
+  return { handleSubmit, successMessage, errorMessage }
 }
