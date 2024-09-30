f46a11f38de6eef784e723815abfc0d928dc9b4e
Conrad Seto <conrad@alephsf.com>
2024-09-30 14:15:18 -0700

    fix: Name of vendor in comments

--- src/app/_lib/api.ts
+++ src/app/_lib/api.ts
@@ -1,5 +1,5 @@
 export function submitToMailChimp(email: string): void {
-  // This function would typically make a POST request to SurveyMonkey API with the email address
+  // This function would typically make a POST request to MailChimp API with the email address
   // For now, we just log it to the console
   console.log('Submitting', email, 'to MailChimp')
 }
