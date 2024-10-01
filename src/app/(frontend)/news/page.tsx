eea39e14755be1fbd72bdfdad1efc05c7675b641
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-10-01 13:26:20 -0700

    feat: Handle no news

--- src/app/(frontend)/news/page.tsx
+++ src/app/(frontend)/news/page.tsx
@@ -19,10 +19,10 @@ export default async function Page() {
     {text: 'Research', link: 'research', id: 5}
   ]
 
-  const featuredPost = news[0]
-  if (!featuredPost) {
+  if (!news || news.length === 0) {
     return <h1 className={styles.pageTitle}>No news posts found</h1>
   }
+  const featuredPost = news[0]
   const {
     slug: featuredPostSlug,
     excerpt: featuredPostExcerpt,
