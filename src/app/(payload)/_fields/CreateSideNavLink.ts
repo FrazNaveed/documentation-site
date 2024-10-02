f6287c625134dfee974e0c85cfbd1a5a9a0d84af
Conrad Seto <conrad@alephsf.com>
2024-10-02 15:30:12 -0700

    refactor: Make Create Side Nav Link checkbox a reusable field

---
+++ src/app/(payload)/_fields/CreateSideNavLink.ts
@@ -0,0 +1,9 @@
+import type { Field } from 'payload'
+
+export const CreateSideNavLinkFields: Field[] = [
+  {
+    name: 'createSideNavLink',
+    type: 'checkbox',
+    defaultValue: false,
+  },
+]
