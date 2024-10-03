0b87cf6e0e8638a3227af3679ae66e71e05e937c
Matt Glaser <matt@alephsf.com>
2024-10-03 10:58:36 -0700

    chore: Add migration from pin to featured

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241001_194129_initial from './20241001_194129_initial';
+import * as migration_20241003_175554 from './20241003_175554';
 
 export const migrations = [
   {
     up: migration_20241001_194129_initial.up,
     down: migration_20241001_194129_initial.down,
-    name: '20241001_194129_initial'
+    name: '20241001_194129_initial',
+  },
+  {
+    up: migration_20241003_175554.up,
+    down: migration_20241003_175554.down,
+    name: '20241003_175554'
   },
 ];
