43fd49aace2ea49d2f2f263460ffc9386b49304a
Conrad Seto <conrad@alephsf.com>
2024-10-04 06:48:38 -0700

    chore: Create migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241001_194129_initial from './20241001_194129_initial';
 import * as migration_20241003_175554 from './20241003_175554';
 import * as migration_20241004_004043 from './20241004_004043';
+import * as migration_20241004_134606 from './20241004_134606';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20241004_004043.up,
     down: migration_20241004_004043.down,
-    name: '20241004_004043'
+    name: '20241004_004043',
+  },
+  {
+    up: migration_20241004_134606.up,
+    down: migration_20241004_134606.down,
+    name: '20241004_134606'
   },
 ];
