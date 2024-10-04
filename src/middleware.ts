0c29ce0620a713d2ee290498c1dd68680d832801
Conrad Seto <conrad@alephsf.com>
2024-10-03 19:45:47 -0700

    fix: Move middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 
-import { i18n } from './i18n-config'
+import { i18n } from './app/i18n-config'
 
 import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
+import { url } from 'inspector'
 
 function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string | string[]> = {}
@@ -22,11 +23,14 @@ function getLocale(request: NextRequest): string | undefined {
 
   const locale = matchLocale(languages, locales, defaultLocale)
 
+  console.log('locale var from getLocale: ', locale)
+
   return locale
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
+  console.log('pathname: ', pathname)
 
   if (
     [
@@ -43,7 +47,8 @@ export function middleware(request: NextRequest) {
 
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request)
-
+    console.warn('path is missing the locale: ', pathname)
+    console.log('THE locale: ', locale)
     return NextResponse.redirect(
       new URL(
         `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
@@ -51,6 +56,17 @@ export function middleware(request: NextRequest) {
       )
     )
   }
+
+
+  // if (pathname.startsWith('/' + i18n.defaultLocale)) {
+  //   console.warn('contains default locale --> remove the locale from the pathname')
+  //   console.log('Rewriting URL without locale...')
+  //   const urlWithoutLocale = pathname.replace(/^\/en/, '')
+  //   console.log('cropped: ', urlWithoutLocale)
+  //   const foo = NextResponse.rewrite(new URL(urlWithoutLocale, request.url))
+  //   console.log('rewritten url: ', foo)
+  //   return NextResponse.rewrite(new URL(urlWithoutLocale, request.url))
+  // }
 }
 
 export const config = {
