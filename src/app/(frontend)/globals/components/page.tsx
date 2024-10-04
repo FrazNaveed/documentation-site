ce4111a4aa5970bebe0784f8c184f8c418d96c5e
Conrad Seto <conrad@alephsf.com>
2024-10-04 08:52:55 -0700

    chore: Fix lint error

--- src/app/(frontend)/globals/components/page.tsx
+++ src/app/(frontend)/globals/components/page.tsx
@@ -2,9 +2,9 @@ import Stats from '../../_components/Stats'
 
 export default function Page() {
   const stats = [
-    {label: 'Flaredrop Apr*', stat: '24.18%' },
-    {label: 'flaredrop + delegating APR*', stat: '30.95%' },
-    {label: 'Flaredrop + Staking APR*', stat: '35.99%' },
+    { label: 'Flaredrop Apr*', stat: '24.18%' },
+    { label: 'flaredrop + delegating APR*', stat: '30.95%' },
+    { label: 'Flaredrop + Staking APR*', stat: '35.99%' },
   ]
 
   const caption = '*APR values are variable and depend on total FLR staked, data provider performance and other factors. See Oracle Daemon to learn more'
