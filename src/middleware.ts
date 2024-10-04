9a219bc3fc4d01d4f1b94bf8c1291fd3d6b09e1f
Conrad Seto <conrad@alephsf.com>
2024-10-03 22:42:38 -0700

    feat: Update middleware to handle admin and possibly internal Payload paths. Rewrite paths containing the default locale.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,72 +1,58 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
-
 import { i18n } from './app/i18n-config'
-
 import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
-function getLocale(request: NextRequest): string | undefined {
-  const negotiatorHeaders: Record<string, string | string[]> = {}
-  request.headers.forEach((value, key) => (
-    negotiatorHeaders[key] = value
-  ))
+function getLocale(request: NextRequest): string {
+  const negotiatorHeaders: Record<string, string> = {}
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
   // @ts-ignore locales are readonly
   const locales: string[] = i18n.locales
-  const defaultLocale = i18n.defaultLocale
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(locales)
+  // console.log('matchLocale: ', matchLocale(languages, locales, i18n.defaultLocale))
 
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-    locales,
-  )
-
-  const locale = matchLocale(languages, locales, defaultLocale)
-
-  console.log('locale var from getLocale: ', locale)
-
-  return locale
+  return matchLocale(languages, locales, i18n.defaultLocale)
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
-  console.log('pathname: ', pathname)
 
-  if (
-    [
-      '/favicon.ico',
-    ].includes(pathname)
-  ) {
+  // Allow direct access to the Payload admin panel and its assets
+  if (pathname.startsWith('/admin') || pathname.startsWith('/_payload')) {
+    const pathWithoutDefaultLocale = pathname.replace(new RegExp(`^/${i18n.defaultLocale}(/|$)`), '/')
+
+    return NextResponse.rewrite(new URL(pathWithoutDefaultLocale, request.url))
+  }
+
+  if (['/favicon.ico'].includes(pathname)) {
     return
   }
 
   const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => {
-      return !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-    })
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  )
 
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request)
-    console.warn('path is missing the locale: ', pathname)
-    console.log('THE locale: ', locale)
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
-        request.url,
-      )
-    )
+    // console.warn('path is missing the locale: ', pathname)
+    // console.log('the locale is: ', locale)
+
+    return NextResponse.rewrite(new URL(`/${locale}${pathname}`, request.url))
   }
 
+  // Handle default locale
+  if (pathname.startsWith(`/${i18n.defaultLocale}`)) {
+    const pathWithoutDefaultLocale = pathname.replace(new RegExp(`^/${i18n.defaultLocale}(/|$)`), '/')
 
-  if (pathname.startsWith('/' + i18n.defaultLocale)) {
-    const urlWithoutLocale = pathname.replace(/^\/en/, '')
+    // console.warn('contains default locale --> remove the locale from the pathname')
+    // console.log('Rewriting URL without locale...')
+    // console.log('path without default locale ', pathWithoutDefaultLocale)
+    const rewrittenUrl = new URL(pathWithoutDefaultLocale, request.url)
+    // console.log('rewritten url: ', rewrittenUrl.href)
 
-    console.warn('contains default locale --> remove the locale from the pathname')
-    console.log('Rewriting URL without locale...')
-    console.log('URL without locale ', urlWithoutLocale)
-    // const rewrittenUrl = `${request.nextUrl.origin}${urlWithoutLocale}`
-    const rewrittenUrl = new URL(urlWithoutLocale, request.url)
-    console.log('rewritten url: ', rewrittenUrl.href)
-    return NextResponse.rewrite(rewrittenUrl)
+    return NextResponse.rewrite(new URL(pathWithoutDefaultLocale, request.url))
   }
 
   return NextResponse.next()
