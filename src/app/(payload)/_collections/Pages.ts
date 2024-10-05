33cb167c97f508df60723c1ac697ce937f326b51
Conrad Seto <conrad@alephsf.com>
2024-10-05 00:01:32 -0700

    feat: Add page banner fields to Pages Collection. Move checkboxes to sidebar.

--- src/app/(payload)/_collections/Pages.ts
+++ src/app/(payload)/_collections/Pages.ts
@@ -3,6 +3,7 @@ import { HeroFields } from '../_fields/HeroFields'
 import { ColumnsBlock } from '../_blocks/ColumnsBlock'
 import { ImageBlock } from '../_blocks/ImageBlock'
 import { RichTextBlock } from 'src/app/(payload)/_blocks/RichTextBlock'
+import { getSiblingData } from 'payload/shared'
 
 export const Pages: CollectionConfig = {
   slug: 'pages',
@@ -32,6 +33,37 @@ export const Pages: CollectionConfig = {
       name: 'hideHero',
       type: 'checkbox',
       defaultValue: false,
+      admin: {
+        position: 'sidebar',
+      },
+    },
+    {
+      name: 'pageBanner',
+      type: 'group',
+      fields: [
+        {
+          name: 'togglePageBanner',
+          type: 'checkbox',
+          defaultValue: false,
+        },
+        {
+          name: 'bannerText',
+          type: 'richText',
+          localized: true,
+          admin: {
+            condition: (data, siblingData, { user }) => {
+              if (siblingData.togglePageBanner) {
+                return true
+              } else {
+                return false
+              }
+            },
+          },
+        },
+      ],
+      admin: {
+        position: 'sidebar',
+      },
     },
     {
       name: 'components',
