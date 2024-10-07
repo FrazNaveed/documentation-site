6234ef100fae1b73b14d09915df46ecb8589825d
Matt Glaser <matt@alephsf.com>
2024-10-07 12:02:49 -0700

    chore: Remove migrations

--- migrations/index.ts
+++ migrations/index.ts
@@ -2,18 +2,6 @@ import * as migration_20241001_194129_initial from './20241001_194129_initial';
 import * as migration_20241003_175554 from './20241003_175554';
 import * as migration_20241004_004043 from './20241004_004043';
 import * as migration_20241004_134606 from './20241004_134606';
-import * as migration_20241004_211919 from './20241004_211919';
-import * as migration_20241004_212654 from './20241004_212654';
-import * as migration_20241004_214839 from './20241004_214839';
-import * as migration_20241004_215030 from './20241004_215030';
-import * as migration_20241004_215222 from './20241004_215222';
-import * as migration_20241005_005012 from './20241005_005012';
-import * as migration_20241005_021210 from './20241005_021210';
-import * as migration_20241005_021751 from './20241005_021751';
-import * as migration_20241005_023147 from './20241005_023147';
-import * as migration_20241005_023235 from './20241005_023235';
-import * as migration_20241005_061202 from './20241005_061202';
-import * as migration_20241005_062044 from './20241005_062044';
 
 export const migrations = [
   {
@@ -35,65 +23,5 @@ export const migrations = [
     up: migration_20241004_134606.up,
     down: migration_20241004_134606.down,
     name: '20241004_134606',
-  },
-  {
-    up: migration_20241004_211919.up,
-    down: migration_20241004_211919.down,
-    name: '20241004_211919',
-  },
-  {
-    up: migration_20241004_212654.up,
-    down: migration_20241004_212654.down,
-    name: '20241004_212654',
-  },
-  {
-    up: migration_20241004_214839.up,
-    down: migration_20241004_214839.down,
-    name: '20241004_214839',
-  },
-  {
-    up: migration_20241004_215030.up,
-    down: migration_20241004_215030.down,
-    name: '20241004_215030',
-  },
-  {
-    up: migration_20241004_215222.up,
-    down: migration_20241004_215222.down,
-    name: '20241004_215222',
-  },
-  {
-    up: migration_20241005_005012.up,
-    down: migration_20241005_005012.down,
-    name: '20241005_005012',
-  },
-  {
-    up: migration_20241005_021210.up,
-    down: migration_20241005_021210.down,
-    name: '20241005_021210',
-  },
-  {
-    up: migration_20241005_021751.up,
-    down: migration_20241005_021751.down,
-    name: '20241005_021751',
-  },
-  {
-    up: migration_20241005_023147.up,
-    down: migration_20241005_023147.down,
-    name: '20241005_023147',
-  },
-  {
-    up: migration_20241005_023235.up,
-    down: migration_20241005_023235.down,
-    name: '20241005_023235',
-  },
-  {
-    up: migration_20241005_061202.up,
-    down: migration_20241005_061202.down,
-    name: '20241005_061202',
-  },
-  {
-    up: migration_20241005_062044.up,
-    down: migration_20241005_062044.down,
-    name: '20241005_062044'
   },
 ];
