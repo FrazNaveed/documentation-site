ab1e7c5e7c5add52d8c07d445288cd81a6d65cd4
Matt Glaser <matt@alephsf.com>
2024-10-07 11:38:51 -0700

    chore(Migration): Manually edit migration

--- migrations/20241005_014644.ts
+++ migrations/20241005_014644.ts
@@ -2,9 +2,10 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
-   ALTER TYPE "enum_pages_hero_style" ADD VALUE 'standard';
+   
   ALTER TABLE "pages" ALTER COLUMN "hero_style" SET DEFAULT 'standard';`)
 }
+// removed ALTER TYPE "enum_pages_hero_style" ADD VALUE 'standard'; from above
 
 export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
