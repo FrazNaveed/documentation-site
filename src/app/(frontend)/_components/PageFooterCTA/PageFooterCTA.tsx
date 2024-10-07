e1079d2c34857320178a2ef01c1e6abacb78c9f3
Conrad Seto <conrad@alephsf.com>
2024-10-07 10:13:23 -0700

    feat: Rough in component

---
+++ src/app/(frontend)/_components/PageFooterCTA/PageFooterCTA.tsx
@@ -0,0 +1,33 @@
+import cx from 'classnames'
+import Button from '../Button'
+import styles from './PageFooterCTA.module.scss'
+
+export type PageFooterCTAProps = {
+  className?: string,
+  buttonText?: string,
+  buttonLink?: string
+}
+
+export default function PageFooterCTA({ className, buttonText, buttonLink }: PageFooterCTAProps) {
+  return (
+    <section className={cx(styles.Wrap, className)}>
+      <div className={styles.content}>
+        <div>
+          foo
+        </div>
+        {buttonText && buttonLink
+          && (
+            <Button
+              text={buttonText}
+              link={buttonLink}
+              size='large'
+              className={styles.PageFooterCTA_Button}
+            />
+          )}
+        <div>
+          bar
+        </div>
+      </div>
+    </section>
+  )
+}
