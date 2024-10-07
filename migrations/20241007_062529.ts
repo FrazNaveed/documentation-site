5dcb3f63d000223e77237e17a2451e07a9ce4767
Conrad Seto <conrad@alephsf.com>
2024-10-06 23:51:54 -0700

    chore: Commit migrations

---
+++ migrations/20241007_062529.ts
@@ -0,0 +1,11 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+   ALTER TABLE "pages_blocks_talking_points" ADD COLUMN "create_side_nav_link" boolean DEFAULT false;`)
+}
+
+export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+   ALTER TABLE "pages_blocks_talking_points" DROP COLUMN IF EXISTS "create_side_nav_link";`)
+}
--- migrations/index.ts
+++ migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20241003_175554 from './20241003_175554';
 import * as migration_20241004_004043 from './20241004_004043';
 import * as migration_20241004_134606 from './20241004_134606';
 import * as migration_20241006_065908 from './20241006_065908';
+import * as migration_20241007_062529 from './20241007_062529';
 
 export const migrations = [
   {
@@ -28,6 +29,11 @@ export const migrations = [
   {
     up: migration_20241006_065908.up,
     down: migration_20241006_065908.down,
-    name: '20241006_065908'
+    name: '20241006_065908',
+  },
+  {
+    up: migration_20241007_062529.up,
+    down: migration_20241007_062529.down,
+    name: '20241007_062529'
   },
 ];
