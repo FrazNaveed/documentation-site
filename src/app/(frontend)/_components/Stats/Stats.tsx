35ebd27a6c3de53a64033947403e107a415a00e4
Conrad Seto <conrad@alephsf.com>
2024-10-08 14:11:10 -0700

    chore: Remove unnecessary wrapping div

--- src/app/(frontend)/_components/Stats/Stats.tsx
+++ src/app/(frontend)/_components/Stats/Stats.tsx
@@ -11,18 +11,16 @@ export type StatsProps = Stats & {
 export default function StatsBlock({ stats, caption, className }: StatsProps) {
   return (
     <section className={cx(styles.statsBlock, className)}>
-      <div className={styles.statsBlockWrap}>
-        <div className={styles.stats}>
-          {stats?.map((stat) => (
-            <div key={stat.id} className={cx(styles.Stat)}>
-              <h3 className={styles.StatLabel}>{stat.label}</h3>
-              <p className={styles.StatNumber}>{stat.stat}</p>
-            </div>
-          ))}
-        </div>
-        <div className={styles.captionWrap}>
-          {caption && <LexicalRenderer content={caption as PayloadLexicalReactRendererContent} />}
-        </div>
+      <div className={styles.stats}>
+        {stats?.map((stat) => (
+          <div key={stat.id} className={cx(styles.Stat)}>
+            <h3 className={styles.StatLabel}>{stat.label}</h3>
+            <p className={styles.StatNumber}>{stat.stat}</p>
+          </div>
+        ))}
+      </div>
+      <div className={styles.captionWrap}>
+        {caption && <LexicalRenderer content={caption as PayloadLexicalReactRendererContent} />}
       </div>
     </section>
   )
