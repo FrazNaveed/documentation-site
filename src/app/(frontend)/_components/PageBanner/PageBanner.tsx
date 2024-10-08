76e30a0e2c094df57c885ac4cd50a87bd940238a
Conrad Seto <conrad@alephsf.com>
2024-10-08 00:51:19 -0700

    chore: Remove async from component

--- src/app/(frontend)/_components/PageBanner/PageBanner.tsx
+++ src/app/(frontend)/_components/PageBanner/PageBanner.tsx
@@ -2,7 +2,7 @@ import LexicalRenderer from 'src/app/(frontend)/_components/LexicalRenderer'
 import type { PayloadLexicalReactRendererContent } from 'src/app/(frontend)/_components/LexicalRenderer/LexicalRenderer'
 import styles from './PageBanner.module.scss'
 
-export default async function PageBanner({ content }:any) {
+export default function PageBanner({ content }:any) {
   return (
     <div className={styles.pageBanner}>
       <div className={styles.pageBannerContent}>
