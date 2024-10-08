3ef72da1b8c0d59ae47eaa8c8822014a62402662
Conrad Seto <conrad@alephsf.com>
2024-10-08 07:51:27 -0700

    chore: Clean up example page and set query for page with slug 'test'

--- src/app/(frontend)/[lang]/globals/components/page.tsx
+++ src/app/(frontend)/[lang]/globals/components/page.tsx
@@ -2,19 +2,9 @@ import StatsBlock from 'src/app/(frontend)/_components/Stats'
 import getStatsBlockFromPage from 'src/app/(frontend)/_lib/payload/statsQueries'
 import { PayloadLexicalReactRendererContent } from 'src/app/(frontend)/_components/LexicalRenderer/LexicalRenderer'
 
-const testStats = await getStatsBlockFromPage('fooz')
-// console.log('testStats: ', testStats)
+const testStats = await getStatsBlockFromPage('test')
 
 export default function Page() {
-  // const stats = [
-  //   { label: 'Flaredrop Apr*', stat: '24.18%', id: '12345' },
-  //   { label: 'flaredrop + delegating APR*', stat: '30.95%', id: '23456' },
-  //   { label: 'Flaredrop + Staking APR*', stat: '35.99%', id: '34678' },
-  // ]
-
-  // eslint-disable-next-line max-len
-  // const caption = '*APR values are variable and depend on total FLR staked, data provider performance and other factors. See Oracle Daemon to learn more'
-
   return (
     <>
       <h1>Components</h1>
