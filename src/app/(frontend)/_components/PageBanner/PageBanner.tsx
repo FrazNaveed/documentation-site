f4e4de879f1a1a2e1fb62fe346d46e6e541303b9
Conrad Seto <conrad@alephsf.com>
2024-10-07 23:46:21 -0700

    chore: Add prop to PageBanner. Type needs work.

--- src/app/(frontend)/_components/PageBanner/PageBanner.tsx
+++ src/app/(frontend)/_components/PageBanner/PageBanner.tsx
@@ -2,9 +2,7 @@ import LexicalRenderer from 'src/app/(frontend)/_components/LexicalRenderer'
 import type { PayloadLexicalReactRendererContent } from 'src/app/(frontend)/_components/LexicalRenderer/LexicalRenderer'
 import styles from './PageBanner.module.scss'
 
-export default async function PageBanner() {
-  const content = null // TODO: fetch data from API
-
+export default async function PageBanner({ content }:any) {
   return (
     <div className={styles.pageBanner}>
       <div className={styles.pageBannerContent}>
