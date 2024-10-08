8d2e8be252556306a293f070ed989aa1806d1ebc
Conrad Seto <conrad@alephsf.com>
2024-10-08 12:48:08 -0700

    fix: Type for icon element. Tweak logic on conditional displaying class names.

--- src/app/(frontend)/_components/TalkingPoints/TalkingPoints.tsx
+++ src/app/(frontend)/_components/TalkingPoints/TalkingPoints.tsx
@@ -1,6 +1,6 @@
 import cx from 'classnames'
 import Image from 'next/image'
-import type { Media, PointsList, ITalkingPoints } from '@/payload-types'
+import type { PointsList, ITalkingPoints } from '@/payload-types'
 import type { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
 import LexicalRenderer from '../LexicalRenderer'
 import styles from './TalkingPoints.module.scss'
@@ -17,29 +17,33 @@ export default function TalkingPoints({
   return (
     <section className={cx(styles.Wrap, className)}>
       <div className={styles.PointsWrap}>
-        {points && points.map((point, index) => (
-          <div
-            key={point.id}
-            className={cx(
-              styles.Point,
-              index % 2 !== 0 ? styles.Point__right : styles.Point__left,
-              index === points.length - 1 && styles.Point__noBorderBottom,
-              (index % 2 === 0 && index === points.length - 2) && styles.Point__noBorderBottom,
-            )}
-          >
-            <div className={styles.PointHeaderWrap}>
-              <Image
-                src={(point.icon as Media).url || ''}
-                width={20}
-                height={20}
-                alt=''
-                className={styles.PointImage}
-              />
-              <div className={styles.PointHeader}>{point.header}</div>
+        {points && points.map((point, index) => {
+          const IconProps = (point.icon && typeof point.icon === 'object' && point.icon.url)
+            ? { src: point.icon.url, alt: point.icon.alt } : { src: '', alt: 'icon' }
+
+          return (
+            <div
+              key={point.id}
+              className={cx(
+                styles.Point,
+                index % 2 !== 0 ? styles.Point__right : styles.Point__left,
+                index === points.length - 1 && styles.Point__noBorderBottom,
+                (index % 2 === 0 && index === points.length - 2) && styles.Point__noBorderBottom,
+              )}
+            >
+              <div className={styles.PointHeaderWrap}>
+                <Image
+                  {...IconProps}
+                  width={20}
+                  height={20}
+                  className={styles.PointImage}
+                />
+                <div className={styles.PointHeader}>{point.header}</div>
+              </div>
+              {point.text && <LexicalRenderer content={point.text as PayloadLexicalReactRendererContent} />}
             </div>
-            {point.text && <LexicalRenderer content={point.text as PayloadLexicalReactRendererContent} />}
-          </div>
-        ))}
+          )
+        })}
       </div>
     </section>
   )
