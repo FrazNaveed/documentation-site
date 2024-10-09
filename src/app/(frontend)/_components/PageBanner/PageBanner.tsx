5a219dc98b5242f232f02e6c5d99b95da453e347
Conrad Seto <conrad@alephsf.com>
2024-10-09 13:09:47 -0700

    feat: Add conditional for displaying component. Add type to component.

--- src/app/(frontend)/_components/PageBanner/PageBanner.tsx
+++ src/app/(frontend)/_components/PageBanner/PageBanner.tsx
@@ -1,8 +1,16 @@
+import type { Page } from 'payload-types'
 import LexicalRenderer from 'src/app/(frontend)/_components/LexicalRenderer'
 import type { PayloadLexicalReactRendererContent } from 'src/app/(frontend)/_components/LexicalRenderer/LexicalRenderer'
 import styles from './PageBanner.module.scss'
 
-export default function PageBanner({ content }:any) {
+interface PageBannerProps {
+  content: NonNullable<Page['pageBanner']>['bannerText'];
+}
+
+export default function PageBanner({ content }: PageBannerProps) {
+  if (!content) {
+    return null
+  }
   return (
     <div className={styles.pageBanner}>
       <div className={styles.pageBannerContent}>
