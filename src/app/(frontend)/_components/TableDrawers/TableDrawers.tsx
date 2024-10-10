8a9247748fb6f9e7c666ece13c2bd1e6409ea2b0
Matt Glaser <matt@alephsf.com>
2024-10-10 11:58:49 -0700

    feat(TableDrawers): Add collapsible drawers for mobile

--- src/app/(frontend)/_components/TableDrawers/TableDrawers.tsx
+++ src/app/(frontend)/_components/TableDrawers/TableDrawers.tsx
@@ -1,9 +1,23 @@
-import { Fragment } from 'react'
+'use client'
+
+import { Fragment, useState } from 'react'
 import { TableWithDrawers } from '@/payload-types'
 import cx from 'classnames'
 import type { PayloadLexicalReactRendererContent } from 'src/app/(frontend)/_components/LexicalRenderer/LexicalRenderer'
 import LexicalRenderer from '../LexicalRenderer'
 import styles from './TableDrawers.module.scss'
+
+// TODO: Replace with SVG or standard icon for plus/minus
+const ShowHideBtn = ({ onClick, collapsed = false }: {onClick: () => void, collapsed: boolean}) => (
+  <button
+    type='button'
+    aria-label='Show/Hide'
+    className={cx(styles.showHideBtn, styles.hideOnTable)}
+    onClick={onClick}
+  >
+    {collapsed ? '+' : '-'}
+  </button>
+)
 
 export default function TableDrawers({ data }: {data: TableWithDrawers}) {
   const {
@@ -12,13 +26,25 @@ export default function TableDrawers({ data }: {data: TableWithDrawers}) {
     sections,
   } = data
 
+  const [collapsedDrawers, setCollapsedDrawers] = useState<number[]>([])
+
+  const handleClick = (indexToCollapse: number) => {
+    setCollapsedDrawers((prev) => {
+      if (prev.includes(indexToCollapse)) {
+        return prev.filter((i) => i !== indexToCollapse)
+      }
+      return [...prev, indexToCollapse]
+    })
+  }
+
   return (
     <section className={cx(styles.container)}>
       <div className={cx(styles.headerRow, styles.row)}>
-        <div className={cx(styles.cell, styles.label)}>
-          {/* Hack. We compress the top section into the first header row */}
+        <div className={cx(styles.cell, styles.label, { [styles.roundBottom]: collapsedDrawers.includes(0) })}>
+          {/* Hack. We compress the top section name into the first header row */}
           <h4 className={styles.sectionHeader}>
             {sections && sections?.length > 0 && sections[0].name}
+            <ShowHideBtn onClick={() => handleClick(0)} collapsed={collapsedDrawers.includes(0)} />
           </h4>
         </div>
         <div className={cx(styles.cell, styles.hideInDrawers)}>
@@ -33,8 +59,11 @@ export default function TableDrawers({ data }: {data: TableWithDrawers}) {
           {/* Hack II: the re-hacking. We hide the header on the first section because of Hack I */}
           {index > 0 && (
             <div className={cx(styles.headerRow, styles.row)}>
-              <div className={cx(styles.cell, styles.label)}>
+              <div
+                className={cx(styles.cell, styles.label, { [styles.roundBottom]: collapsedDrawers.includes(index) })}
+              >
                 <h4 className={styles.sectionHeader}>{name}</h4>
+                <ShowHideBtn onClick={() => handleClick(index)} collapsed={collapsedDrawers.includes(index)} />
               </div>
               <div className={cx(styles.cell, styles.hideInDrawers)} />
               <div className={cx(styles.cell, styles.hideInDrawers)} />
@@ -44,9 +73,14 @@ export default function TableDrawers({ data }: {data: TableWithDrawers}) {
             {rows && rows.map(({
               // eslint-disable-next-line no-shadow
               rowLabel, column1Data, column2Data, id,
-            // eslint-disable-next-line no-shadow
-            }, index) => (
-              <div className={cx(styles.row, { [styles.rowLast]: index === rows.length - 1 })} key={id}>
+            }, rowIndex) => (
+              <div
+                className={cx(styles.row, {
+                  [styles.rowLast]: rowIndex === rows.length - 1,
+                  [styles.collapsed]: collapsedDrawers.includes(index),
+                })}
+                key={id}
+              >
                 <div className={cx(styles.cell, styles.label, { [styles.hideInDrawers]: !rowLabel })}>
                   {rowLabel && <h4 className={styles.labelText}>{rowLabel}</h4>}
                 </div>
