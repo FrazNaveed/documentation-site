f98c2528ed77cdd897b8a44c9257fbbca68b08a6
Conrad Seto <conrad@alephsf.com>
2024-10-09 22:09:40 -0700

    feat: Update logic to check for existing slug value

--- src/app/(payload)/_utils/setSlugFromTitle.ts
+++ src/app/(payload)/_utils/setSlugFromTitle.ts
@@ -2,6 +2,10 @@ import type { FieldHook } from 'payload'
 import slugify from 'slugify'
 
 const setSlugFromTitle: FieldHook = ({ data, originalDoc }) => {
+  if (data?.slug) {
+    return
+  }
+
   if (data?.title) {
     return slugify(data.title, { lower: true })
   }
