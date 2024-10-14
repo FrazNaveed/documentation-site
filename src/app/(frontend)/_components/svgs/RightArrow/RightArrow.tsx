b9f7537b78ace575d22c2555baaf1df876462839
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-10-14 16:14:40 -0700

    feat: Add aria-hidden to RightArrow

--- src/app/(frontend)/_components/svgs/RightArrow/RightArrow.tsx
+++ src/app/(frontend)/_components/svgs/RightArrow/RightArrow.tsx
@@ -1,5 +1,5 @@
 const RightArrow = ({ className }: { className?: string }): React.JSX.Element => (
-  <svg className={className} width='21' height='21' viewBox='0 0 21 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <svg className={className} width='21' height='21' viewBox='0 0 21 21' fill='none' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
     <path d='M16.5431 11.1254L0.189453 11.1254L0.189453 9.10262L16.5431 9.10262L8.86155 1.42102L10.3035 0L20.4175 10.114L10.3035 20.228L8.86155 18.807L16.5431 11.1254Z' fill='#C10F45' />
   </svg>
 )
