a48cf00b959597d81d900eab54f6f27aac9f2359
Conrad Seto <conrad@alephsf.com>
2024-10-13 21:23:27 -0700

    chore: Rename component

---
+++ src/app/(frontend)/_components/TeamGridBlock/TeamGridBlock.tsx
@@ -0,0 +1,16 @@
+// import Image from 'next/image'
+// import styles from './TeamGridBlock.module.scss'
+
+export default function TeamGridBlock({ title, team }: any) {
+  console.log(team)
+  return (
+    <section>
+      <div>
+        <h2>{title}</h2>
+      </div>
+      <div>
+        {/* grid */}
+      </div>
+    </section>
+  )
+}
