6ef5c8dbb59a50c6fecb8b1143c7e8172671b974
Conrad Seto <conrad@alephsf.com>
2024-10-13 19:24:57 -0700

    feat: Rough in TeamGrid component

---
+++ src/app/(frontend)/_components/TeamGrid/TeamGrid.tsx
@@ -0,0 +1,12 @@
+export default function TeamGrid() {
+  return (
+    <section>
+      <div>
+        <h2>The Team</h2>
+      </div>
+      <div>
+        {/* grid */}
+      </div>
+    </section>
+  )
+}
