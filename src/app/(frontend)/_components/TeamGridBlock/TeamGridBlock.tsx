c95da23ecded7feac7e89b7fd53587bf6446e2cf
Conrad Seto <conrad@alephsf.com>
2024-10-14 08:36:11 -0700

    feat: Finalize markup and classes

--- src/app/(frontend)/_components/TeamGridBlock/TeamGridBlock.tsx
+++ src/app/(frontend)/_components/TeamGridBlock/TeamGridBlock.tsx
@@ -1,16 +1,36 @@
-// import Image from 'next/image'
-// import styles from './TeamGridBlock.module.scss'
+import Image from 'next/image'
+import styles from './TeamGridBlock.module.scss'
 
 export default function TeamGridBlock({ title, team }: any) {
-  console.log(team)
   return (
-    <section>
-      <div>
-        <h2>{title}</h2>
+    <section className={styles.teamGridBlock}>
+      <div className={styles.titleWrap}>
+        <h2 className={styles.title}>{title}</h2>
       </div>
-      <div>
-        {/* grid */}
-      </div>
+      <ul className={styles.teamGridWrap}>
+        {team?.map((teamMember: any) => {
+          const {
+            fullName,
+            headshot,
+            id,
+            jobTitle,
+          } = teamMember
+          return (
+            <li key={id}>
+              <div className={styles.imgWrap}>
+                <Image
+                  src={headshot.url}
+                  alt={headshot.alt}
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <h3 className={styles.name}>{fullName}</h3>
+              <p className={styles.jobTitle}>{jobTitle}</p>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
