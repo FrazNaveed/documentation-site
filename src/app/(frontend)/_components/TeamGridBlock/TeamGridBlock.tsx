a5e08b2f3391565ba731b61b8f720eaa9187dfc9
Conrad Seto <conrad@alephsf.com>
2024-10-15 16:34:20 -0700

    chore: Add check for empty team

--- src/app/(frontend)/_components/TeamGridBlock/TeamGridBlock.tsx
+++ src/app/(frontend)/_components/TeamGridBlock/TeamGridBlock.tsx
@@ -13,32 +13,36 @@ export default function TeamGridBlock({ gridTitle, team }: TeamGridProps) {
       <div className={styles.titleWrap}>
         <h2 className={styles.title}>{gridTitle}</h2>
       </div>
-      <ul className={styles.teamGridWrap}>
-        {team?.map((teamMember: Person) => {
-          const {
-            fullName,
-            headshot,
-            id,
-            jobTitle,
-          } = teamMember
-          return (
-            <li key={id} className={styles.teamGridMember}>
-              {(headshot && typeof headshot === 'object' && headshot.url) && (
-                <div className={styles.imgWrap}>
-                  <Image
-                    src={headshot?.url}
-                    alt={headshot?.alt}
-                    width={184}
-                    height={245}
-                  />
-                </div>
-              )}
-              <h3 className={styles.name}>{fullName}</h3>
-              <p className={styles.jobTitle}>{jobTitle}</p>
-            </li>
-          )
-        })}
-      </ul>
+      {team?.length && team?.length > 0 ? (
+        <ul className={styles.teamGridWrap}>
+          {team?.map((teamMember: Person) => {
+            const {
+              fullName,
+              headshot,
+              id,
+              jobTitle,
+            } = teamMember
+            return (
+              <li key={id} className={styles.teamGridMember}>
+                {(headshot && typeof headshot === 'object' && headshot.url) && (
+                  <div className={styles.imgWrap}>
+                    <Image
+                      src={headshot?.url}
+                      alt={headshot?.alt}
+                      width={184}
+                      height={245}
+                    />
+                  </div>
+                )}
+                <h3 className={styles.name}>{fullName}</h3>
+                <p className={styles.jobTitle}>{jobTitle}</p>
+              </li>
+            )
+          })}
+        </ul>
+      ) : (
+        <p>No team members to display.</p>
+      )}
     </section>
   )
 }
