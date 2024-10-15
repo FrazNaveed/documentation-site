3863eb0ed63d0c0de9d5193611fcc55aff00c907
Conrad Seto <conrad@alephsf.com>
2024-10-15 16:25:36 -0700

    chore: Typescript fun time

--- src/app/(frontend)/_components/TeamGridBlock/TeamGridBlock.tsx
+++ src/app/(frontend)/_components/TeamGridBlock/TeamGridBlock.tsx
@@ -1,14 +1,20 @@
 import Image from 'next/image'
+import type { Person } from '@/payload-types'
 import styles from './TeamGridBlock.module.scss'
 
-export default function TeamGridBlock({ title, team }: any) {
+export type TeamGridProps = {
+  gridTitle?: string | null | undefined
+  team?: Person[]
+}
+
+export default function TeamGridBlock({ gridTitle, team }: TeamGridProps) {
   return (
     <section className={styles.teamGridBlock}>
       <div className={styles.titleWrap}>
-        <h2 className={styles.title}>{title}</h2>
+        <h2 className={styles.title}>{gridTitle}</h2>
       </div>
       <ul className={styles.teamGridWrap}>
-        {team?.map((teamMember: any) => {
+        {team?.map((teamMember: Person) => {
           const {
             fullName,
             headshot,
@@ -17,14 +23,16 @@ export default function TeamGridBlock({ title, team }: any) {
           } = teamMember
           return (
             <li key={id} className={styles.teamGridMember}>
-              <div className={styles.imgWrap}>
-                <Image
-                  src={headshot.url}
-                  alt={headshot.alt}
-                  width={184}
-                  height={245}
-                />
-              </div>
+              {(headshot && typeof headshot === 'object' && headshot.url) && (
+                <div className={styles.imgWrap}>
+                  <Image
+                    src={headshot?.url}
+                    alt={headshot?.alt}
+                    width={184}
+                    height={245}
+                  />
+                </div>
+              )}
               <h3 className={styles.name}>{fullName}</h3>
               <p className={styles.jobTitle}>{jobTitle}</p>
             </li>
