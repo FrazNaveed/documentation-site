07d20fbbd305b2a6949005a845426db672686360
Conrad Seto <conrad@alephsf.com>
2024-10-16 14:08:23 -0700

    feat: Add markup

--- src/app/(frontend)/_components/ProductGrid/ProductGrid.tsx
+++ src/app/(frontend)/_components/ProductGrid/ProductGrid.tsx
@@ -1,16 +1,32 @@
 // import cx from 'classnames'
+import Link from 'next/link'
+import ForwardArrow from '../svgs/ForwardArrow'
 import styles from './ProductGrid.module.scss'
 
 export type ProductGridProps = {
   title?: string
+  products: any
 }
 
-export default function ProductGrid({ title }: ProductGridProps) {
+export default function ProductGrid({ title, products }: ProductGridProps) {
   return (
     <section className={styles.productGrid}>
       <h2 className={styles.productGridTitle}>{title}</h2>
       <div className={styles.productGridWrap}>
-        map products
+        {products?.map((product: any) => (
+          <Link href={product.slug} key={product.id} className={styles.productWrap}>
+            <div className={styles.product}>
+              <div className={styles.productHeader}>
+                <div className={styles.productInfo}>
+                  <p>uploaded icon</p>
+                  <h3 className={styles.productTitle}>{product.title}</h3>
+                </div>
+                <ForwardArrow />
+              </div>
+              <p className={styles.productDescription}>{product.description}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </section>
   )
