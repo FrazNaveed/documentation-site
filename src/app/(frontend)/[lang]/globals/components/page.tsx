d2cdbe1ccb5c5c8056ea0823307f6b4bcaa91699
Conrad Seto <conrad@alephsf.com>
2024-10-17 12:25:00 -0700

    chore: Add test version to globals page

--- src/app/(frontend)/[lang]/globals/components/page.tsx
+++ src/app/(frontend)/[lang]/globals/components/page.tsx
@@ -1,15 +1,38 @@
-import getStatsBlockFromPage from 'src/app/(frontend)/_lib/payload/statsQueries'
+// import getStatsBlockFromPage from 'src/app/(frontend)/_lib/payload/statsQueries'
 import LinkBand from '../../../_components/LinkBand'
 
-const testStats = await getStatsBlockFromPage('test')
-console.log('testStats', testStats)
+// const testStats = await getStatsBlockFromPage('test')
+// console.log('testStats', testStats)
+const title = 'Exporers & Resources'
+const links = [
+  {
+    linkText: 'foo',
+    linkUrl: 'https://foo.com',
+    id: '1',
+  },
+  {
+    linkText: 'bar',
+    linkUrl: 'https://bar.com',
+    id: '2',
+  },
+  {
+    linkText: 'baz',
+    linkUrl: 'https://baz.com',
+    id: '3',
+  },
+  {
+    linkText: 'qux',
+    linkUrl: 'https://qux.com',
+    id: '4',
+  },
+]
 
 export default function Page() {
   return (
     <>
       <h1>Components</h1>
       test components here
-      <LinkBand />
+      <LinkBand title={title} links={links} />
     </>
   )
 }
