ae5400e91053dfc79e1c954b7fe7234d1c815f8d
Conrad Seto <conrad@alephsf.com>
2024-10-17 12:07:12 -0700

    feat: Add markup

--- src/app/(frontend)/_components/LinkBand/LinkBand.tsx
+++ src/app/(frontend)/_components/LinkBand/LinkBand.tsx
@@ -1,9 +1,30 @@
+import cx from 'classnames'
+import Link from 'next/link'
+import DiagonalArrowSquare from '../svgs/DiagonalArrowSquare'
 import styles from './LinkBand.module.scss'
 
-export default function LinkBand() {
+export default function LinkBand({ title, links }: any) {
   return (
     <section className={styles.linkBand}>
-      LINK BANDDDD
+      <div className={styles.linkBandHeader}>
+        <h2 className={styles.linkBandTitle}>{title}</h2>
+      </div>
+      <div className={styles.linkBandLinks}>
+        {links.map((link: any, index: number) => (
+          <Link
+            key={link.id}
+            href={link.linkUrl}
+            className={cx(
+              styles.linkBandLink,
+              index === links.length - 1 && styles.linkBandLink__noBorderBottom,
+              (index % 2 === 0 && index === links.length - 2) && styles.linkBandLink__noBorderBottom,
+            )}
+          >
+            {link.linkText}
+            <DiagonalArrowSquare />
+          </Link>
+        ))}
+      </div>
     </section>
   )
 }
