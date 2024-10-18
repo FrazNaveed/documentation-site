5fa0126e9ea5771c3a9be5bb366f73c0ec63e97d
Conrad Seto <conrad@alephsf.com>
2024-10-17 23:23:51 -0700

    feat: Add images section

--- src/app/(frontend)/_components/TallCTA/TallCta.tsx
+++ src/app/(frontend)/_components/TallCTA/TallCta.tsx
@@ -1,18 +1,33 @@
-// import Image from 'next/image'
+import Image from 'next/image'
+import { i18n } from '@/src/app/i18n-config'
 import Button from '../Button'
-// import DiagonalArrowSquare from '../svgs/DiagonalArrowSquare'
 import styles from './TallCta.module.scss'
 
 export default function TallCta({
-  title, content, buttonText, buttonLink,
+  title, content, buttonText, buttonLink, option,
 }: any) {
   return (
     <section className={styles.tallCta}>
       <div className={styles.tallCtaWrap}>
-        <div>
-          image
+        <div className={styles.tallCtaImageWrap}>
+          <Image
+            src={`/${i18n.defaultLocale}/bug.png`}
+            alt='bg image'
+            width={592}
+            height={592}
+            className={styles.tallCtaImage}
+          />
+          {option && (
+            <Image
+              src={`/${i18n.defaultLocale}/bug.png`}
+              alt='bg image'
+              width={230}
+              height={230}
+              className={styles.tallCtaOptionalImage}
+            />
+          )}
         </div>
-        <div>
+        <div className={styles.tallCtaContent}>
           <h2>{title}</h2>
           <p>{content}</p>
           <Button
