5f1f5035836fc6d1b3436b5912f4d671c555da79
Conrad Seto <conrad@alephsf.com>
2024-10-18 19:39:28 -0700

    feat: Add props to control display of header link, add markup

--- src/app/(frontend)/_components/RelatedPosts/RelatedPosts.tsx
+++ src/app/(frontend)/_components/RelatedPosts/RelatedPosts.tsx
@@ -1,15 +1,37 @@
 import { News } from '@/payload-types'
-import styles from './RelatedPosts.module.scss'
+import Link from 'next/link'
+import RightArrow from '../svgs/RightArrow'
 import TeaserGrid from '../TeaserGrid'
+import styles from './RelatedPosts.module.scss'
 
 export type RelatedPostsProps = {
+  header?: string | 'Related News'
+  linkText?: string | undefined
+  linkUrl?: string | undefined
   posts: News[]
 }
 
-export default function RelatedPosts({ posts }: RelatedPostsProps) {
+export default function RelatedPosts({
+  header = 'Related News',
+  posts,
+  linkText,
+  linkUrl,
+}: RelatedPostsProps) {
   return (
     <div className={styles.relatedNews}>
-      <h5 className={styles.relatedNewsHeader}>Related News</h5>
+      <div className={styles.relatedNewsHeaderWrap}>
+        <h5 className={styles.relatedNewsHeader}>{header}</h5>
+        {linkText && linkUrl && (
+          <Link
+            href={linkUrl}
+          >
+            <span className={styles.relatedNewsLink}>
+              {linkText}
+              <RightArrow className={styles.arrowRight} />
+            </span>
+          </Link>
+        )}
+      </div>
       <TeaserGrid teasers={posts} />
     </div>
   )
