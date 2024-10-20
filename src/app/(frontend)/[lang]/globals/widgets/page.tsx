4fa37c7e517f9343bd3ab1f8aab65f8fab4c0207
Conrad Seto <conrad@alephsf.com>
2024-10-19 20:57:12 -0700

    chore: Remove from globals page

--- src/app/(frontend)/[lang]/globals/widgets/page.tsx
+++ src/app/(frontend)/[lang]/globals/widgets/page.tsx
@@ -2,34 +2,6 @@ import { TableWithDrawers } from '@/payload-types'
 import FlareDropDates from '../../../_components/FlaredropDates/FlareDropDates'
 import TableDrawers from '../../../_components/TableDrawers'
 import tabledata from '../../../_components/TableDrawers/example.json'
-import ProductGrid from '../../../_components/ProductGrid'
-
-const products = [
-  {
-    title: 'foo',
-    id: '1',
-    slug: '/foo',
-    description: 'Deploy a Hello World contract on Flare, using only your browser',
-  },
-  {
-    title: 'bar',
-    id: '2',
-    slug: '/bar',
-    description: 'High-integrity, block latency data feeds for decentralized finance on Flare',
-  },
-  {
-    title: 'baz',
-    id: '3',
-    slug: '/baz',
-    description: 'Verifiable, tamper-proof Web2 & Web3 data for real-world applications on Flare',
-  },
-  {
-    title: 'qux',
-    id: '4',
-    slug: '/qux',
-    description: 'Verifiable economic security enabling defi for BTC, XRP and DOGE on Flare',
-  },
-]
 
 export default function Page() {
   return (
@@ -37,7 +9,6 @@ export default function Page() {
       <h2>Widgets!</h2>
       <FlareDropDates />
       <TableDrawers data={tabledata as TableWithDrawers} />
-      <ProductGrid title='Title' products={products} />
     </>
   )
 }
