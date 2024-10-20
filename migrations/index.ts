f3f7adc46bd8ae94e57f349c3d78c69a5c513200
Conrad Seto <conrad@alephsf.com>
2024-10-20 08:40:37 -0700

    chore: Commit migration files

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241018_190104 from './20241018_190104';
 import * as migration_20241018_195345 from './20241018_195345';
 import * as migration_20241019_010728 from './20241019_010728';
+import * as migration_20241020_154000 from './20241020_154000';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20241019_010728.up,
     down: migration_20241019_010728.down,
-    name: '20241019_010728'
+    name: '20241019_010728',
+  },
+  {
+    up: migration_20241020_154000.up,
+    down: migration_20241020_154000.down,
+    name: '20241020_154000'
   },
 ];
