2551480ce8ecc77c700a8e16de87577d13d67f5e
Conrad Seto <conrad@alephsf.com>
2024-10-19 20:30:40 -0700

    feat: Create fields for Dev Hub Page Template

---
+++ src/app/(payload)/_fields/DevHub.ts
@@ -0,0 +1,27 @@
+import type { Field } from  'payload'
+import { relationship } from 'payload/shared'
+
+export const DevHub: Field[] = [
+  {
+    name: 'devHub',
+    type: 'group',
+    label: 'Dev Hub Template',
+    interfaceName: 'DevHub',
+    fields: [
+      {
+        name: 'productsGrid',
+        label: 'Products Grid',
+        type: 'relationship',
+        relationTo: 'products',
+        hasMany: true,
+        required: false,
+      },
+    ],
+    admin: {
+      description: 'Settings for the Dev Hub Page Template. Manage content using the Products Collection.',
+      condition: (data, siblingData, { user }) => {
+        return siblingData.pageTemplate === 'devHub'
+       },
+    },
+  }
+]
