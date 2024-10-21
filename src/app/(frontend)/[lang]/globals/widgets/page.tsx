a7cd7f65330ba647987ce57df46d60a2d566015a
Conrad Seto <conrad@alephsf.com>
2024-10-21 09:07:15 -0700

    chore: Move LinkBand test component. Delete global components page.

--- src/app/(frontend)/[lang]/globals/widgets/page.tsx
+++ src/app/(frontend)/[lang]/globals/widgets/page.tsx
@@ -2,11 +2,37 @@ import { TableWithDrawers } from '@/payload-types'
 import FlareDropDates from '../../../_components/FlaredropDates/FlareDropDates'
 import TableDrawers from '../../../_components/TableDrawers'
 import tabledata from '../../../_components/TableDrawers/example.json'
+import LinkBand from '../../../_components/LinkBand'
+
+const title = 'Exporers & Resources'
+const links = [
+  {
+    linkText: 'foo',
+    linkUrl: 'https://foo.com',
+    id: '1',
+  },
+  {
+    linkText: 'bar',
+    linkUrl: 'https://bar.com',
+    id: '2',
+  },
+  {
+    linkText: 'baz',
+    linkUrl: 'https://baz.com',
+    id: '3',
+  },
+  {
+    linkText: 'qux',
+    linkUrl: 'https://qux.com',
+    id: '4',
+  },
+]
 
 export default function Page() {
   return (
     <>
       <h2>Widgets!</h2>
+      <LinkBand title={title} links={links} />
       <FlareDropDates />
       <TableDrawers data={tabledata as TableWithDrawers} />
     </>
