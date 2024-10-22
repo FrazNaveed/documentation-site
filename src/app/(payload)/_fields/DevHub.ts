316c1d19c8cf1b1b2907f4d16d51fa3922675632
Conrad Seto <conrad@alephsf.com>
2024-10-21 21:31:23 -0700

    feat: Add fields for Link Band to Payload

--- src/app/(payload)/_fields/DevHub.ts
+++ src/app/(payload)/_fields/DevHub.ts
@@ -15,6 +15,34 @@ export const DevHub: Field[] = [
         hasMany: true,
         required: false,
       },
+      {
+        name: 'linkBand',
+        type: 'group',
+        label: 'Link Band',
+        fields: [
+          {
+            name: 'linkBandTitle',
+            type: 'text',
+          },
+          {
+            name: 'links',
+            type: 'array',
+            fields: [
+              {
+                name: 'linkText',
+                type: 'text',
+              },
+              {
+                name: 'linkUrl',
+                type: 'text',
+              },
+            ],
+          },
+        ],
+        admin: {
+          description: 'Fill in Link Band Title to override default title "Explorers & Resources"',
+        },
+      }
     ],
     admin: {
       description: 'Settings for the Dev Hub Page Template. Manage content using the Products Collection.',
