7d999aac49e9d622f1f7b54114698770a8b49fb3
Conrad Seto <conrad@alephsf.com>
2024-10-22 16:42:18 -0700

    feat: Add markup for secondary button

--- src/app/(frontend)/_components/PageFooterCTA/PageFooterCTA.tsx
+++ src/app/(frontend)/_components/PageFooterCTA/PageFooterCTA.tsx
@@ -7,7 +7,9 @@ import styles from './PageFooterCTA.module.scss'
 export type PageFooterCTAProps = {
   className?: string,
   buttonText?: string,
-  buttonLink?: string
+  buttonLink?: string,
+  buttonSecondaryText?: string,
+  buttonSecondaryLink?: string,
   backgroundImage?: (number | null) | Media
   backgroundImageStyle: ('flipped' | 'offset') | null
 }
@@ -16,6 +18,8 @@ export default function PageFooterCTA({
   className,
   buttonText,
   buttonLink,
+  buttonSecondaryText,
+  buttonSecondaryLink,
   backgroundImage,
   backgroundImageStyle,
 }: PageFooterCTAProps) {
@@ -23,15 +27,27 @@ export default function PageFooterCTA({
     <section className={cx(styles.Wrap, className)}>
       <div className={styles.content}>
         <PageFooterImage backgroundImage={backgroundImage} backgroundImageStyle={backgroundImageStyle} backgroundImagePosition='left' />
-        {buttonText && buttonLink
-          && (
-            <Button
-              text={buttonText}
-              link={buttonLink}
-              size='large'
-              className={styles.Button}
-            />
-          )}
+        <div className={styles.buttonWrap}>
+          {buttonText && buttonLink
+            && (
+              <Button
+                text={buttonText}
+                link={buttonLink}
+                size='large'
+                className={styles.Button}
+              />
+            )}
+          {buttonSecondaryText && buttonSecondaryLink
+            && (
+              <Button
+                text={buttonSecondaryText}
+                link={buttonSecondaryLink}
+                size='large'
+                buttonStyle='secondary'
+                className={styles.Button}
+              />
+            )}
+        </div>
         <PageFooterImage backgroundImage={backgroundImage} backgroundImageStyle={backgroundImageStyle} backgroundImagePosition='right' />
       </div>
     </section>
