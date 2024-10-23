4bd8ce58a6f33f65b1f87afc180cf81b8140b734
Conrad Seto <conrad@alephsf.com>
2024-10-23 09:54:19 -0700

    Revert "chore: Add IF NOT EXISTS to migrations"
    This reverts commit f609b0d2ceb4e476e78b6cfeceb19aac8b0d0745.

--- migrations/20241023_154313.ts
+++ migrations/20241023_154313.ts
@@ -2,10 +2,10 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
-   ALTER TABLE "pages" ADD COLUMN IF NOT EXISTS "hero_button_secondary_link" varchar;
-  ALTER TABLE "pages" ADD COLUMN IF NOT EXISTS "page_footer_c_t_a_button_button_secondary_link" varchar;
-  ALTER TABLE "pages_locales" ADD COLUMN IF NOT EXISTS "hero_button_secondary_text" varchar;
-  ALTER TABLE "pages_locales" ADD COLUMN IF NOT EXISTS "page_footer_c_t_a_button_button_secondary_text" varchar;`)
+   ALTER TABLE "pages" ADD COLUMN "hero_button_secondary_link" varchar;
+  ALTER TABLE "pages" ADD COLUMN "page_footer_c_t_a_button_button_secondary_link" varchar;
+  ALTER TABLE "pages_locales" ADD COLUMN "hero_button_secondary_text" varchar;
+  ALTER TABLE "pages_locales" ADD COLUMN "page_footer_c_t_a_button_button_secondary_text" varchar;`)
 }
 
 export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
