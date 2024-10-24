7f8a829260889262dfcf88e2ae0d2c67622f1103
Conrad Seto <conrad@alephsf.com>
2024-10-24 10:18:04 -0700

    feat: Create component

--- src/app/(frontend)/_components/ImageTextGridBlock/ImageTextGridBlock.tsx
+++ src/app/(frontend)/_components/ImageTextGridBlock/ImageTextGridBlock.tsx
@@ -1,10 +1,45 @@
+import Image from 'next/image'
+import type { ImageTextCards } from '@/payload-types'
+import LexicalRenderer from '../LexicalRenderer'
 import styles from './ImageTextGridBlock.module.scss'
+import { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
 
-export default function ImageTextGridBlock() {
+export type ImageTextGridBlockProps = {
+  imageTextGridTitle?: string | null
+  imageTextCardGrid?: ImageTextCards | []
+}
+
+export default function ImageTextGridBlock({ imageTextGridTitle, imageTextCardGrid }: ImageTextGridBlockProps) {
+  console.log('cards: ', imageTextCardGrid)
   return (
-    <section>
-      <div className={styles.header}>
-        <h2>Image Text Grid</h2>
+    <section className={styles.imageTextGridBlock}>
+      <div className={styles.headerWrap}>
+        <h2>{imageTextGridTitle}</h2>
+      </div>
+      <div className={styles.imageTextGridWrap}>
+        <ul className={styles.imageTextGrid}>
+          {imageTextCardGrid?.map((card) => {
+            const {
+              cardHeader, cardImage, cardText, id,
+            } = card
+            return (
+              <li key={id} className={styles.imageTextGridCard}>
+                {cardImage && typeof cardImage === 'object' && cardImage.url && cardImage.alt && (
+                  <div className={styles.imageWrap}>
+                    <Image
+                      src={cardImage.url}
+                      alt={cardImage.alt}
+                      width={340}
+                      height={254}
+                    />
+                  </div>
+                )}
+                <h3 className={styles.imageTextGridCardHeader}>{cardHeader}</h3>
+                {cardText && <LexicalRenderer content={cardText as PayloadLexicalReactRendererContent} />}
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </section>
   )
