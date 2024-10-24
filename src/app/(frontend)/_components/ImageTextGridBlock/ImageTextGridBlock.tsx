d3048f130bb42d78af9cf6640c7c747128a44c01
Conrad Seto <conrad@alephsf.com>
2024-10-24 15:38:12 -0700

    chore: Remove entire grid field to be added back

--- src/app/(frontend)/_components/ImageTextGridBlock/ImageTextGridBlock.tsx
+++ src/app/(frontend)/_components/ImageTextGridBlock/ImageTextGridBlock.tsx
@@ -1,27 +1,27 @@
 // import Image from 'next/image'
-import type { ImageTextCards } from '@/payload-types'
-import LexicalRenderer from '../LexicalRenderer'
+// import type { ImageTextCards } from '@/payload-types'
+// import LexicalRenderer from '../LexicalRenderer'
 import styles from './ImageTextGridBlock.module.scss'
-import { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
+// import { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
 
 export type ImageTextGridBlockProps = {
   imageTextGridTitle?: string | null
-  imageTextCardGrid?: ImageTextCards | []
+  // imageTextCardGrid?: ImageTextCards | []
 }
 
-export default function ImageTextGridBlock({ imageTextGridTitle, imageTextCardGrid }: ImageTextGridBlockProps) {
+export default function ImageTextGridBlock({ imageTextGridTitle }: ImageTextGridBlockProps) {
   return (
     <section className={styles.imageTextGridBlock}>
       <h2 className={styles.imageTextGridTitle}>{imageTextGridTitle}</h2>
       <div className={styles.imageTextGridWrap}>
         <ul className={styles.imageTextGrid}>
-          {imageTextCardGrid?.map((card) => {
+          {/* {imageTextCardGrid?.map((card) => {
             const {
               cardHeader, cardText, id,
             } = card
             return (
               <li key={id} className={styles.imageTextGridCard}>
-                {/* {cardImage && typeof cardImage === 'object' && cardImage.url && cardImage.alt && (
+                {cardImage && typeof cardImage === 'object' && cardImage.url && cardImage.alt && (
                   <div className={styles.imageWrap}>
                     <Image
                       src={cardImage.url}
@@ -30,12 +30,12 @@ export default function ImageTextGridBlock({ imageTextGridTitle, imageTextCardGr
                       height={254}
                     />
                   </div>
-                )} */}
+                )}
                 <h3 className={styles.imageTextGridCardHeader}>{cardHeader}</h3>
                 {cardText && <LexicalRenderer content={cardText as PayloadLexicalReactRendererContent} />}
               </li>
             )
-          })}
+          })} */}
         </ul>
       </div>
     </section>
