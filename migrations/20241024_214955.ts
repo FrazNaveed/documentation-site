64d89352a789cc96ec20566e4aaf602ad5a73f5a
Conrad Seto <conrad@alephsf.com>
2024-10-24 14:51:56 -0700

    chore: Edit migration

--- migrations/20241024_214955.ts
+++ migrations/20241024_214955.ts
@@ -2,8 +2,8 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
-   ALTER TABLE "pages_blocks_image_text_grid_image_text_card_grid" ADD COLUMN "card_image_id" integer;
-  ALTER TABLE "pages_blocks_image_text_grid_locales" ADD COLUMN "image_text_grid_title" varchar;
+   ALTER TABLE "pages_blocks_image_text_grid_image_text_card_grid" ADD COLUMN IF NOT EXISTS "card_image_id" integer;
+  ALTER TABLE "pages_blocks_image_text_grid_locales" ADD COLUMN IF NOT EXISTS "image_text_grid_title" varchar;
   DO $$ BEGIN
    ALTER TABLE "pages_blocks_image_text_grid_image_text_card_grid" ADD CONSTRAINT "pages_blocks_image_text_grid_image_text_card_grid_card_image_id_media_id_fk" FOREIGN KEY ("card_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
   EXCEPTION
