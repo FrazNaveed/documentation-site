55f37eb136542348f7499511fdbac501c6999814
Conrad Seto <conrad@alephsf.com>
2024-10-25 08:51:12 -0700

    chore: Remove array and migrate

--- src/app/(frontend)/_components/ImageTextGridBlock/ImageTextGridBlock.tsx
+++ src/app/(frontend)/_components/ImageTextGridBlock/ImageTextGridBlock.tsx
@@ -1,20 +1,20 @@
-import Image from 'next/image'
-import type { ImageTextCards } from '@/payload-types'
-import LexicalRenderer from '../LexicalRenderer'
+// import Image from 'next/image'
+// import type { ImageTextCards } from '@/payload-types'
+// import LexicalRenderer from '../LexicalRenderer'
 import styles from './ImageTextGridBlock.module.scss'
-import { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
+// import { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
 
 export type ImageTextGridBlockProps = {
   imageTextGridTitle?: string | null
-  imageTextCardGrid?: ImageTextCards | []
+  // imageTextCardGrid?: ImageTextCards | []
 }
 
-export default function ImageTextGridBlock({ imageTextGridTitle, imageTextCardGrid }: ImageTextGridBlockProps) {
+export default function ImageTextGridBlock({ imageTextGridTitle }: ImageTextGridBlockProps) {
   return (
     <section className={styles.imageTextGridBlock}>
       <h2 className={styles.imageTextGridTitle}>{imageTextGridTitle}</h2>
       <div className={styles.imageTextGridWrap}>
-        <ul className={styles.imageTextGrid}>
+        {/* <ul className={styles.imageTextGrid}>
           {imageTextCardGrid?.map((card) => {
             const {
               cardHeader, cardImage, cardText, id,
@@ -40,7 +40,7 @@ export default function ImageTextGridBlock({ imageTextGridTitle, imageTextCardGr
               )
             )
           })}
-        </ul>
+        </ul> */}
       </div>
     </section>
   )
