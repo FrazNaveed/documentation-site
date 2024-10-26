97e01ac5bc78b02a992800f8584c9ce9ad142bfd
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-10-25 20:27:49 -0700

    feat: Dybamically update grant flags based on width

--- src/app/(frontend)/_components/FeaturedGrants/FeaturedGrants.tsx
+++ src/app/(frontend)/_components/FeaturedGrants/FeaturedGrants.tsx
@@ -1,6 +1,6 @@
-import * as flags from 'country-flag-icons/react/3x2'
 import Pill from 'src/app/(frontend)/_components/Pill'
 import type { Grants } from '@/payload-types'
+import FeaturedGrantsCountries from './FeaturedGrantsCountries'
 import styles from './FeaturedGrants.module.scss'
 
 export type FeaturedGrantsProps = {
@@ -12,8 +12,6 @@ export default function FeaturedGrants({ grants }: FeaturedGrantsProps) {
     return null
   }
   const { grantsAwarded, countries, topCategories } = grants
-  const countriesLength = countries?.length
-  const maxFlagsToShow = 10
   return (
     <div className={styles.grants}>
       <div className={styles.sectionOne}>
@@ -26,27 +24,7 @@ export default function FeaturedGrants({ grants }: FeaturedGrantsProps) {
         <p className={styles.sectionLabel}>
           Countries
         </p>
-        {countries && (
-          <div className={styles.countries}>
-            <p className={styles.countryCount}>{countriesLength}</p>
-            <div className={styles.flags}>
-              {countries.slice(0, maxFlagsToShow).map((countryObj) => {
-                const { country } = countryObj
-                const FlagComponent = flags[country]
-                return FlagComponent && (
-                  <div className={styles.flag}>
-                    <FlagComponent title={country} className={styles.flag_Icon} />
-                  </div>
-                )
-              })}
-              {(countriesLength as number) > maxFlagsToShow && (
-                <div className={styles.extraCountryCount}>
-                  {`+${(countriesLength as number) - maxFlagsToShow}`}
-                </div>
-              )}
-            </div>
-          </div>
-        )}
+        {countries && <FeaturedGrantsCountries countries={countries} />}
       </div>
       <div className={styles.sectionThree}>
         <p className={styles.sectionLabel}>
@@ -55,14 +33,14 @@ export default function FeaturedGrants({ grants }: FeaturedGrantsProps) {
         {topCategories && (
           <div className={styles.categories}>
             {topCategories.map((category) => {
-              const { type, number } = category
+              const { id, type, number } = category
               if (!type || typeof type !== 'object') {
                 return null
               }
               const { title } = type
               const pillText = number ? `${title} (${number})` : title
               return (
-                <div className={styles.category}>
+                <div key={id} className={styles.category}>
                   <Pill text={pillText} size='small' />
                 </div>
               )
