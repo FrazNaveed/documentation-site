3f555d328080f0bdb2c05755484aea077b618e9d
Conrad Seto <conrad@alephsf.com>
2024-10-29 08:59:05 -0700

    chore: Fix sizes again

--- src/app/(frontend)/_components/TwoColumnBlock/TwoColumnBlock.tsx
+++ src/app/(frontend)/_components/TwoColumnBlock/TwoColumnBlock.tsx
@@ -41,7 +41,7 @@ export default function TwoColumnBlock({
                   alt={column.image.alt}
                   width={column.image.width ?? 0}
                   height={column.image.height ?? 0}
-                  sizes='(max-width: 768px) 25vw, (max-width: 1024px) 12.5vw, 8.33vw'
+                  sizes='(max-width: 768px) 25vw, (max-width: 1024px) 12.5vw, 25vw'
                   style={{
                     objectFit: column.imageFill || 'contain',
                     objectPosition: column.imageAlignment || 'center',
