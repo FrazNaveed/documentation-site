b747a50837aef083ed491eb4ed163d2c9e141333
Conrad Seto <conrad@alephsf.com>
2024-10-29 12:50:08 -0700

    feat: Add grid markup

--- src/app/(frontend)/_components/PastFeaturedGrantsGridBlock/PastFeaturedGrantsGridBlock.tsx
+++ src/app/(frontend)/_components/PastFeaturedGrantsGridBlock/PastFeaturedGrantsGridBlock.tsx
@@ -1,9 +1,66 @@
+import type { Grant } from '@/payload-types'
+import Image from 'next/image'
+import Link from 'next/link'
+import type { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
 import styles from './PastFeaturedGrantsGridBlock.module.scss'
+import LexicalRenderer from '../LexicalRenderer'
 
-export default function PastFeaturedGrantsGridBlock() {
+export type PastFeaturedGrantsGridBlockProps = {
+  gridTitle?: string | null
+  grantsGrid?: (number | Grant)[] | null
+}
+
+export default function PastFeaturedGrantsGridBlock({ gridTitle, grantsGrid }: PastFeaturedGrantsGridBlockProps) {
   return (
-    <section className={styles.Wrap}>
-      <h2>Past Featured Grants</h2>
+    <section className={styles.wrap}>
+      {gridTitle && <h2 className={styles.gridTitle}>{gridTitle}</h2>}
+      <div className={styles.grantsGrid}>
+        {grantsGrid?.map((grant: any) => {
+          const {
+            name,
+            description,
+            id,
+            logo,
+            country,
+            announcementLink,
+            grantCategory,
+          } = grant
+          return (
+            <li key={id} className={styles.grant}>
+              <div>
+                {/* <Link href='/'> */}
+                {(logo || name) && (
+                  <div className={styles.grantHeader}>
+                    {logo && typeof logo === 'object' && logo.url
+                      && (
+                      <div>
+                        <Image
+                          src={logo.url}
+                          alt={logo.alt}
+                          width={logo.width ?? 0}
+                          height={logo.height ?? 0}
+                        />
+                      </div>
+                      )}
+                    {name && <h2 className={styles.name}>{name}</h2>}
+                  </div>
+                )}
+                {description && <LexicalRenderer content={description as PayloadLexicalReactRendererContent} />}
+                <div className={styles.grantFooter}>
+                  {grantCategory?.map((category: any) => (
+                    <p>{category.title}</p>
+                  ))}
+                  {country}
+                  {announcementLink && (
+                    <Link href={announcementLink}>YO</Link>
+                  )}
+                </div>
+                {/* </Link> */}
+              </div>
+            </li>
+          )
+        })}
+      </div>
     </section>
   )
 }
