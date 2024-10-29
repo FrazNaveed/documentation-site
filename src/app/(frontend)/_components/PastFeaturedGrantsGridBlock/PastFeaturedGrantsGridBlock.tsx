638d13fb63abf07530654e28bed070d3f77b28d5
Conrad Seto <conrad@alephsf.com>
2024-10-29 05:41:43 -0700

    fat: Set up component

---
+++ src/app/(frontend)/_components/PastFeaturedGrantsGridBlock/PastFeaturedGrantsGridBlock.tsx
@@ -0,0 +1,9 @@
+import styles from './PastFeaturedGrantsGridBlock.module.scss'
+
+export default function PastFeaturedGrantsGridBlock() {
+  return (
+    <section className={styles.Wrap}>
+      <h2>Past Featured Grants</h2>
+    </section>
+  )
+}
