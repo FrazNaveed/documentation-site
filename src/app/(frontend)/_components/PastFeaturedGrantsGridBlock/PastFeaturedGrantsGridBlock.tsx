019d06f5b2ebd3ea02e9194844cc686e5a192d90
Conrad Seto <conrad@alephsf.com>
2024-10-29 21:33:59 -0700

    feat: Finalize markup. Render flag icon

--- src/app/(frontend)/_components/PastFeaturedGrantsGridBlock/PastFeaturedGrantsGridBlock.tsx
+++ src/app/(frontend)/_components/PastFeaturedGrantsGridBlock/PastFeaturedGrantsGridBlock.tsx
@@ -1,6 +1,11 @@
+// 'use client'
+
+import cx from 'classnames'
+// import { useState } from 'react'
+import * as flags from 'country-flag-icons/react/3x2'
 import type { Grant } from '@/payload-types'
 import Image from 'next/image'
-import Link from 'next/link'
+import ExternalLink from '../ExternalLink'
 import type { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
 import styles from './PastFeaturedGrantsGridBlock.module.scss'
 import LexicalRenderer from '../LexicalRenderer'
@@ -10,12 +15,16 @@ export type PastFeaturedGrantsGridBlockProps = {
   grantsGrid?: (number | Grant)[] | null
 }
 
+type CountryFlags = {
+  [key: string]: React.FC<{ title?: string, className?: string}>
+}
+
 export default function PastFeaturedGrantsGridBlock({ gridTitle, grantsGrid }: PastFeaturedGrantsGridBlockProps) {
   return (
     <section className={styles.wrap}>
       {gridTitle && <h2 className={styles.gridTitle}>{gridTitle}</h2>}
       <div className={styles.grantsGrid}>
-        {grantsGrid?.map((grant: any) => {
+        {grantsGrid?.map((grant :any, index) => {
           const {
             name,
             description,
@@ -25,39 +34,53 @@ export default function PastFeaturedGrantsGridBlock({ gridTitle, grantsGrid }: P
             announcementLink,
             grantCategory,
           } = grant
+          const FlagComponent = (flags as CountryFlags)[country]
           return (
-            <li key={id} className={styles.grant}>
-              <div>
-                {/* <Link href='/'> */}
-                {(logo || name) && (
-                  <div className={styles.grantHeader}>
-                    {logo && typeof logo === 'object' && logo.url
-                      && (
-                      <div>
-                        <Image
-                          src={logo.url}
-                          alt={logo.alt}
-                          width={logo.width ?? 0}
-                          height={logo.height ?? 0}
-                        />
-                      </div>
-                      )}
-                    {name && <h2 className={styles.name}>{name}</h2>}
-                  </div>
-                )}
-                {description && <LexicalRenderer content={description as PayloadLexicalReactRendererContent} />}
-                <div className={styles.grantFooter}>
-                  {grantCategory?.map((category: any) => (
-                    <p>{category.title}</p>
-                  ))}
-                  {country}
-                  {announcementLink && (
-                    <Link href={announcementLink}>YO</Link>
+            <>
+              <li key={id} className={cx(styles.grant, index >= 6 && styles.collapsed)}>
+                <div>
+                  {(logo || name) && (
+                    <div className={styles.grantHeader}>
+                      {logo && typeof logo === 'object' && logo.url
+                        && (
+                        <div className={styles.grantLogoWrap}>
+                          <Image
+                            src={logo.url}
+                            alt={logo.alt}
+                            width={logo.width ?? 0}
+                            height={logo.height ?? 0}
+                          />
+                        </div>
+                        )}
+                      {name && <h2 className={styles.name}>{name}</h2>}
+                    </div>
                   )}
+                  <div className={styles.grantDescription}>
+                    {description && <LexicalRenderer content={description as PayloadLexicalReactRendererContent} />}
+                  </div>
+                  <div className={styles.grantFooter}>
+                    <div className={styles.grantFooterTags}>
+                      {grantCategory && (
+                        <div className={styles.grantCategoryWrap}>
+                          {grantCategory?.map((category: any) => (
+                            <span key={id} className={styles.grantCategoryTag}>{category.title}</span>
+                          ))}
+                        </div>
+                      )}
+                      {FlagComponent && (
+                        <div className={styles.flagWrap}>
+                          <FlagComponent title={country} className={styles.flagIcon} />
+                        </div>
+                      )}
+                    </div>
+                    {announcementLink && (
+                      <ExternalLink href={announcementLink}>Announcement</ExternalLink>
+                    )}
+                  </div>
                 </div>
-                {/* </Link> */}
-              </div>
-            </li>
+              </li>
+              {index === 5 && <span>Show button here</span>}
+            </>
           )
         })}
       </div>
