3a51e452be6075d77d0c596dd619fb776a049df4
Matt Glaser <matt@alephsf.com>
2024-10-30 10:49:57 -0700

    feat(Home): Add initial test animation to home page

--- src/app/(frontend)/[lang]/page.tsx
+++ src/app/(frontend)/[lang]/page.tsx
@@ -1,5 +1,16 @@
+import styles from './page.module.scss'
+
 export default function Page() {
   return (
-    <h1>Hello, Page!</h1>
+    <div className={styles.wrap}>
+      <div className={styles.heroWrap}>
+        <div className={styles.hero}>
+          <video className={styles.heroBg} autoPlay loop muted>
+            <source src='/en/video/57.mp4' type='video/mp4' />
+          </video>
+          <h3 className={styles.heroHeader}>The Blockchain for Data</h3>
+        </div>
+      </div>
+    </div>
   )
 }
