d0adb89ce911c4c112b09f36da7b7ed8c63819f0
Conrad Seto <conrad@alephsf.com>
2024-10-29 22:15:00 -0700

    feat: Add button interactivity. To-do: Toggle cards

--- src/app/(frontend)/_components/PastFeaturedGrantsGridBlock/PastFeaturedGrantsGridBlock.tsx
+++ src/app/(frontend)/_components/PastFeaturedGrantsGridBlock/PastFeaturedGrantsGridBlock.tsx
@@ -1,7 +1,7 @@
-// 'use client'
+'use client'
 
 import cx from 'classnames'
-// import { useState } from 'react'
+import { useState } from 'react'
 import * as flags from 'country-flag-icons/react/3x2'
 import type { Grant } from '@/payload-types'
 import Image from 'next/image'
@@ -20,6 +20,11 @@ type CountryFlags = {
 }
 
 export default function PastFeaturedGrantsGridBlock({ gridTitle, grantsGrid }: PastFeaturedGrantsGridBlockProps) {
+  const [allShown, setAllShown] = useState(false)
+  const displayShowButton = grantsGrid && grantsGrid.length > 3
+  const toggleAllShown = () => {
+    setAllShown((prev) => !prev)
+  }
   return (
     <section className={styles.wrap}>
       {gridTitle && <h2 className={styles.gridTitle}>{gridTitle}</h2>}
@@ -36,12 +41,11 @@ export default function PastFeaturedGrantsGridBlock({ gridTitle, grantsGrid }: P
           } = grant
           const FlagComponent = (flags as CountryFlags)[country]
           return (
-            <>
-              <li key={id} className={cx(styles.grant, index >= 6 && styles.collapsed)}>
-                <div>
-                  {(logo || name) && (
-                    <div className={styles.grantHeader}>
-                      {logo && typeof logo === 'object' && logo.url
+            <li key={id} className={cx(styles.grant, index >= 4 && styles.collapsed)}>
+              <div>
+                {(logo || name) && (
+                <div className={styles.grantHeader}>
+                  {logo && typeof logo === 'object' && logo.url
                         && (
                         <div className={styles.grantLogoWrap}>
                           <Image
@@ -52,37 +56,46 @@ export default function PastFeaturedGrantsGridBlock({ gridTitle, grantsGrid }: P
                           />
                         </div>
                         )}
-                      {name && <h2 className={styles.name}>{name}</h2>}
+                  {name && <h2 className={styles.name}>{name}</h2>}
+                </div>
+                )}
+                <div className={styles.grantDescription}>
+                  {description && <LexicalRenderer content={description as PayloadLexicalReactRendererContent} />}
+                </div>
+                <div className={styles.grantFooter}>
+                  <div className={styles.grantFooterTags}>
+                    {grantCategory && (
+                    <div className={styles.grantCategoryWrap}>
+                      {grantCategory?.map((category: any) => (
+                        <span key={category.id} className={styles.grantCategoryTag}>{category.title}</span>
+                      ))}
                     </div>
-                  )}
-                  <div className={styles.grantDescription}>
-                    {description && <LexicalRenderer content={description as PayloadLexicalReactRendererContent} />}
-                  </div>
-                  <div className={styles.grantFooter}>
-                    <div className={styles.grantFooterTags}>
-                      {grantCategory && (
-                        <div className={styles.grantCategoryWrap}>
-                          {grantCategory?.map((category: any) => (
-                            <span key={id} className={styles.grantCategoryTag}>{category.title}</span>
-                          ))}
-                        </div>
-                      )}
-                      {FlagComponent && (
-                        <div className={styles.flagWrap}>
-                          <FlagComponent title={country} className={styles.flagIcon} />
-                        </div>
-                      )}
+                    )}
+                    {FlagComponent && (
+                    <div className={styles.flagWrap}>
+                      <FlagComponent title={country} className={styles.flagIcon} />
                     </div>
-                    {announcementLink && (
-                      <ExternalLink href={announcementLink}>Announcement</ExternalLink>
                     )}
                   </div>
+                  {announcementLink && (
+                  <ExternalLink href={announcementLink}>Announcement</ExternalLink>
+                  )}
                 </div>
-              </li>
-              {index === 5 && <span>Show button here</span>}
-            </>
+              </div>
+            </li>
           )
         })}
+        {displayShowButton && (
+        <div className={styles.buttonWrap}>
+          <button
+            type='button'
+            className={styles.button}
+            onClick={toggleAllShown}
+          >
+            {allShown ? '- Show Less' : '+ Show All'}
+          </button>
+        </div>
+        )}
       </div>
     </section>
   )
