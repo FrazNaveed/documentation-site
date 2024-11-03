f1fabad74c3ec87236acc1c62aba7a8fe08ffeb2
Conrad Seto <conrad@alephsf.com>
2024-11-03 07:09:57 -0800

    fix: Remove duplicate entry

--- migrations/20241103_062223.ts
+++ migrations/20241103_062223.ts
@@ -2,13 +2,11 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
-  ALTER TYPE "public"."enum_pages_blocks_talking_points_variation" ADD VALUE 'textualGrid' BEFORE 'wideList';
-  ALTER TABLE "pages_blocks_official_channels_locales" ADD COLUMN "text" jsonb;`)
+  ALTER TYPE "public"."enum_pages_blocks_talking_points_variation" ADD VALUE 'textualGrid' BEFORE 'wideList';`)
 }
 
 export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
-  ALTER TABLE "pages_blocks_official_channels_locales" DROP COLUMN IF EXISTS "text";
   ALTER TABLE "public"."pages_blocks_talking_points" ALTER COLUMN "variation" SET DATA TYPE text;
   DROP TYPE "public"."enum_pages_blocks_talking_points_variation";
   CREATE TYPE "public"."enum_pages_blocks_talking_points_variation" AS ENUM('standard', 'wideList');
