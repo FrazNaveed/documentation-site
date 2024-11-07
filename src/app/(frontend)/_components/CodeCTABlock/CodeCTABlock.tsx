1d9b8d70a1fe53cb765e60e8b662233206a5314b
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-11-06 17:24:19 -0800

    feat: Add CodeCTABlock

---
+++ src/app/(frontend)/_components/CodeCTABlock/CodeCTABlock.tsx
@@ -0,0 +1,92 @@
+import Image from 'next/image'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { codeToHtml } from 'shiki'
+// eslint-disable-next-line camelcase
+import { IBM_Plex_Mono } from 'next/font/google'
+import cx from 'classnames'
+import type { ICodeCta } from '@/payload-types'
+import Button from 'src/app/(frontend)/_components/Button'
+import LexicalRenderer from 'src/app/(frontend)/_components/LexicalRenderer'
+import type { PayloadLexicalReactRendererContent } from 'src/app/(frontend)/_components/LexicalRenderer/LexicalRenderer'
+import CodeCTATabs from './CodeCTATabs'
+import codeExamples from './codeExamples'
+import styles from './CodeCTABlock.module.scss'
+
+const ibmPlexMono = IBM_Plex_Mono({
+  weight: '500',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export default async function CodeCTABlock({
+  header,
+  text,
+  buttonText,
+  buttonLink,
+  buttonSecondaryText,
+  buttonSecondaryLink,
+  hideCode,
+  image,
+}: ICodeCta) {
+  const hasButton = buttonText && buttonLink
+  const hasSecondaryButton = buttonSecondaryText && buttonSecondaryLink
+  const hasTextContent = header || text || hasButton || hasSecondaryButton
+  const showImage = hideCode && image && typeof image === 'object' && image.url
+  if (!hasTextContent && hideCode && !showImage) {
+    return null
+  }
+  return (
+    <section className={styles.wrap}>
+      <div className={cx(styles.grid, { [styles.grid__withImage]: showImage })}>
+        {hasTextContent && (
+          <div className={styles.contentCol}>
+            {header && <h3 className={styles.header}>{header}</h3>}
+            {text && (
+              <div className={styles.text}>
+                <LexicalRenderer content={text as PayloadLexicalReactRendererContent} />
+              </div>
+            )}
+            {(hasButton || hasSecondaryButton) && (
+              <div className={styles.buttons}>
+                {hasButton && <Button text={buttonText} link={buttonLink} />}
+                {hasSecondaryButton && <Button text={buttonSecondaryText} link={buttonSecondaryLink} buttonStyle='secondary' />}
+              </div>
+            )}
+          </div>
+        )}
+        {!hideCode && (
+          <div className={styles.codeCol}>
+            <CodeCTATabs labels={codeExamples.map((codeExample) => codeExample.language)} />
+            <div className={styles.codeExamples}>
+              {codeExamples.map(async (codeExample, i) => {
+                const codeHighlighted = await codeToHtml(codeExample.code, {
+                  lang: codeExample.languageId,
+                  theme: 'github-light',
+                })
+                return (
+                  <div
+                    key={codeExample.language}
+                    className={cx(styles.code, styles[`code__${i}`], ibmPlexMono.className)}
+                    dangerouslySetInnerHTML={{ __html: codeHighlighted }}
+                  />
+                )
+              })}
+            </div>
+          </div>
+        )}
+        {(showImage && image.url) && (
+          <div className={styles.imageWrap}>
+            <Image
+              className={styles.image}
+              src={image.url}
+              width={image.width ?? 0}
+              height={image.height ?? 0}
+              alt={image.alt}
+              priority
+            />
+          </div>
+        )}
+      </div>
+    </section>
+  )
+}
---
+++ src/app/(frontend)/_components/CodeCTABlock/codeExamples.ts
@@ -0,0 +1,116 @@
+export default [
+  {
+    language: 'On-Chain',
+    languageId: 'solidity',
+    code: `
+      import "@flarenetwork/flare-periphery-contracts/flare/ContractRegistry.sol";
+      import "@flarenetwork/flare-periphery-contracts/flare/FtsoV2Interface.sol";
+
+      contract FtsoV2Consumer {
+          FtsoV2Interface internal ftsoV2;
+
+          constructor() {
+              ftsoV2 = ContractRegistry.getFtsoV2();
+          }
+
+          function getFlrUsdPrice() external returns 
+              (uint256 value, int8 decimals, uint64 timestamp) {
+              return ftsoV2.getFeedById(0x01464c522f55534400000000000000000000000000);
+          }
+      }
+    `,
+  },
+  {
+    language: 'Javascript',
+    languageId: 'js',
+    code: `
+      import { Web3 } from "web3";
+
+      export async function main() {
+        const w3 = new Web3(RPC_URL);
+        const ftsov2 = new w3.eth.Contract(JSON.parse(ABI), FTSOV2_ADDRESS);
+
+        const res = await ftsov2.methods
+          .getFeedById("0x01464c522f55534400000000000000000000000000")
+          .call();
+
+        console.log(
+          "Value: %s, Decimals: %s, Timestamp: %s", res["0"], res["1"], res["2"],
+        );
+      }
+    `,
+  },
+  {
+    language: 'Python',
+    languageId: 'py',
+    code: `
+      import asyncio
+      from web3 import AsyncHTTPProvider, AsyncWeb3
+
+
+      async def main() -> None:
+          w3 = AsyncWeb3(AsyncHTTPProvider(RPC_URL))
+          ftsov2 = w3.eth.contract(address=FTSOV2_ADDRESS, abi=ABI)
+
+          res = await ftsov2.functions.getFeedById(
+              "0x01464c522f55534400000000000000000000000000"
+          ).call()
+
+          print(f"Value: {res[0]}, Decimals: {res[1]}, Timestamp {res[2]}")
+    `,
+  },
+  {
+    language: 'Rust',
+    languageId: 'rs',
+    code: `
+      use alloy::{providers::ProviderBuilder, sol};
+      use eyre::Result;
+
+      sol!(
+          FtsoV2, 
+          "abi/FtsoV2.json"
+      );
+
+      #[tokio::main]
+      async fn main() -> Result<()> {
+          let provider = ProviderBuilder::new().on_http(RPC_URL);
+          let ftsov2 = FtsoV2::new(FTSOV2_ADDRESS, provider);
+
+          let FtsoV2::getFeedByIdReturn { _0, _1, _2 } = ftsov2
+              .getFeedById("0x01464c522f55534400000000000000000000000000")
+              .call()
+              .await?;
+
+          println!("Value:{}, Decimals:{}, Timestamp{}", _0, _1, _2);
+          Ok(())
+      }
+    `,
+  },
+  {
+    language: 'Go',
+    languageId: 'go',
+    code: `
+      package main
+
+      import (
+        "context"
+        "fmt"
+        "github.com/ethereum/go-ethereum/accounts/abi/bind"
+        "github.com/ethereum/go-ethereum/ethclient"
+      )
+
+      func FtsoV2Consumer() {
+        client, _ := ethclient.Dial(RPC_URL)
+        ftsov2, _ := NewFtsoV2(FTSOV2_ADDRESS, client)
+        var res []interface{}
+        opts := &bind.CallOpts{Context: context.Background()}
+
+        ftsov2.FtsoV2Caller.contract.Call(
+          opts, &res, "getFeedById", "0x01464c522f55534400000000000000000000000000"
+        )
+        
+        fmt.Println("Value: %i, Decimals: %i, Timestamp: %i", res[0], res[1], res[2])
+      }
+    `,
+  },
+]
