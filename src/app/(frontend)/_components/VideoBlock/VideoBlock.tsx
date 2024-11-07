d0cf3c0e6d1162e989c802aa4bdd55e1a1a4e78f
Conrad Seto <conrad@alephsf.com>
2024-11-07 15:26:33 -0800

    feat: Add className prop and replace utility function for margin classes VideoBlock

--- src/app/(frontend)/_components/VideoBlock/VideoBlock.tsx
+++ src/app/(frontend)/_components/VideoBlock/VideoBlock.tsx
@@ -3,7 +3,6 @@
 import dynamic from 'next/dynamic'
 import cx from 'classnames'
 import styles from './VideoBlock.module.scss'
-import applyBlockMarginStyles from '../../_utils/applyBlockMarginStyles'
 
 const ReactPlayer = dynamic(() => import('react-player/lazy'), {
   ssr: false,
@@ -12,15 +11,14 @@ const ReactPlayer = dynamic(() => import('react-player/lazy'), {
 export type VideoBlockProps = {
   title?: string | null,
   url?: string | null,
-  standardBottomMargin?: boolean | null,
-  standardTopMargin?: boolean | null,
+  className?: string
 }
 
 export default function VideoBlock({
-  title, url, standardBottomMargin, standardTopMargin,
+  title, url, className,
 }: VideoBlockProps) {
   return (
-    <section className={cx(styles.videoBlockWrap, applyBlockMarginStyles(standardTopMargin, standardBottomMargin))}>
+    <section className={cx(styles.videoBlockWrap, className)}>
       {title && <h2 className={styles.videoBlockTitle}>{title}</h2>}
       {url && (
         <div className={styles.videoPlayerWrap}>
