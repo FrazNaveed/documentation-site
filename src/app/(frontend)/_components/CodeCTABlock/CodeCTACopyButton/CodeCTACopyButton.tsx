63da82e546f7f5766841a397a2e347cccee13f75
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-11-06 20:16:46 -0800

    feat: Clear timeout on unmount

--- src/app/(frontend)/_components/CodeCTABlock/CodeCTACopyButton/CodeCTACopyButton.tsx
+++ src/app/(frontend)/_components/CodeCTABlock/CodeCTACopyButton/CodeCTACopyButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from 'src/app/(frontend)/_components/Button'
 import styles from '../CodeCTABlock.module.scss'
 
@@ -10,10 +10,19 @@ type CodeCTACopyButtonProps = {
 
 export default function CodeCTACopyButton({ codeToCopy }: CodeCTACopyButtonProps) {
   const [buttonText, setButtonText] = useState('Copy')
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>()
+
+  useEffect(() => () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+  }, [timeoutId])
+
   const copyCodeClick = () => {
     navigator.clipboard.writeText(codeToCopy)
     setButtonText('Copied')
-    setTimeout(() => setButtonText('Copy'), 3000)
+    const timeout = setTimeout(() => setButtonText('Copy'), 3000)
+    setTimeoutId(timeout)
   }
   return (
     <Button className={styles.copyButton} type='button' onClick={copyCodeClick} text={buttonText} buttonStyle='secondary' size='small' />
