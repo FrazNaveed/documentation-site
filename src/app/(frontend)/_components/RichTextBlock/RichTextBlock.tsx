7f06a834ae931a97812ec0f1813db7c6d92a55cd
Conrad Seto <conrad@alephsf.com>
2024-11-07 15:09:50 -0800

    feat: Add className prop and replace utility function for margin classes RichTextBlock

--- src/app/(frontend)/_components/RichTextBlock/RichTextBlock.tsx
+++ src/app/(frontend)/_components/RichTextBlock/RichTextBlock.tsx
@@ -1,22 +1,20 @@
 import cx from 'classnames'
 import LexicalRenderer from 'src/app/(frontend)/_components/LexicalRenderer'
 import type { PayloadLexicalReactRendererContent } from 'src/app/(frontend)/_components/LexicalRenderer/LexicalRenderer'
-import type { RichText as IRichText, RichTextBlock as IRichTextBlock } from 'payload-types'
+import type { RichText as IRichText } from 'payload-types'
 import styles from './RichTextBlock.module.scss'
-import applyBlockMarginStyles from '../../_utils/applyBlockMarginStyles'
 
 type RichTextBlockProps = {
   richText: IRichText['richText'],
-  standardTopMargin?: IRichTextBlock['standardTopMargin']
-  standardBottomMargin?: IRichTextBlock['standardBottomMargin']
+  className?: string,
 }
 
-export default function RichTextBlock({ richText, standardBottomMargin, standardTopMargin }: RichTextBlockProps) {
+export default function RichTextBlock({ richText, className }: RichTextBlockProps) {
   if (!richText) {
     return null
   }
   return (
-    <div className={cx(styles.wrap, applyBlockMarginStyles(standardTopMargin, standardBottomMargin))}>
+    <div className={cx(styles.wrap, className)}>
       <div className={styles.cols}>
         <LexicalRenderer content={richText as PayloadLexicalReactRendererContent} />
       </div>
