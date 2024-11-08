997ed61c51a73285dbbd19fa7578fa87cc1a3951
Conrad Seto <conrad@alephsf.com>
2024-11-07 16:48:45 -0800

    chore: Add block margins to ImageBlock

--- src/app/(payload)/_blocks/ImageBlock/index.ts
+++ src/app/(payload)/_blocks/ImageBlock/index.ts
@@ -1,6 +1,7 @@
 import { Block } from 'payload'
 import { CreateSideNavLinkFields } from '../../_fields/CreateSideNavLink'
 import { i18n } from '../../../i18n-config'
+import { BlockMarginSettings } from '../../_fields/BlockMarginSettings'
 
 export const ImageBlock: Block = {
   slug: 'image',
@@ -12,6 +13,7 @@ export const ImageBlock: Block = {
       relationTo: 'media',
     },
     ...CreateSideNavLinkFields,
+    ...BlockMarginSettings,
   ],
   imageURL: `/${i18n.defaultLocale}/icons/image.svg`,
   imageAltText: 'Image block icon',
