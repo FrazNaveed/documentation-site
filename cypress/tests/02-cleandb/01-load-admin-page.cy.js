fbe6fd5d9060bac117d3d5ff40b763a2d5fa1def
Aljaz Starc <aljaz@flare.network>
2024-11-10 12:33:26 +0000

    update the e2e spec

--- cypress/tests/02-cleandb/01-load-admin-page.cy.js
+++ cypress/tests/02-cleandb/01-load-admin-page.cy.js
@@ -1,29 +1,39 @@
 /* eslint-disable no-undef */
 
 const usermail = "aljaz@flare.network"
-const userpass = Cypress._.random(0, 1e6);
+const userpass = "superduper123"
 
 describe('base checks', () => {
   it('loads admin page', () => {
     cy.visit("/admin")
   })
 
-  
-  it("creates first admin user", () => {
+
+  it("creates first admin user if needed", () => {
     cy.visit("/admin")
-    cy.get('form[action="/api/users/first-register"]')
-    cy.get('input#field-email').type(usermail)
-    cy.get('input#field-password').type(userpass)
-    cy.get('input#field-confirm-password').type(userpass)
-    cy.get('form[action="/api/users/first-register"]').submit()
+
+    cy.get("body", { log: false }).then(body => {
+      if (body.find('form[action="/api/users/first-register"]').length == 1) {
+        cy.get('input#field-email').type(usermail)
+        cy.get('input#field-password').type(userpass)
+        cy.get('input#field-confirm-password').type(userpass)
+        cy.wait(1000)
+        cy.get('form[action="/api/users/first-register"]').submit()
+        cy.get('a[href="/admin/logout"]').should('exist').wait(1500).click()
+        cy.get('button[type="submit"]').contains('Login')
+      }
+    })    
   })
 
   it("logs in as admin user", () => {
-      cy.visit("/admin")
-      cy.get('form[action="/api/users/login"]')
-      cy.get('input#field-email').type(usermail)
-      cy.get('input#field-password').type(userpass)
-      cy.get('form[action="/api/users/login"]').submit()
+    cy.visit("/admin")
+    cy.wait(1000)
+    cy.get('form[action="/api/users/login"]')
+    cy.get('input#field-email').type(usermail)
+    cy.get('input#field-password').type(userpass)
+    cy.get('form[action="/api/users/login"]').submit()
+
+    cy.url().should('contain', '/admin')
   })
 
 
