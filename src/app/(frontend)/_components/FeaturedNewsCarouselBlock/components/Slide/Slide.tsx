aa5d688167e2ac9ab50ba9b5876aafcb68031c13
Conrad Seto <conrad@alephsf.com>
2024-11-12 15:19:18 -0800

    feat: Add slide elements from teaser

--- src/app/(frontend)/_components/FeaturedNewsCarouselBlock/components/Slide/Slide.tsx
+++ src/app/(frontend)/_components/FeaturedNewsCarouselBlock/components/Slide/Slide.tsx
@@ -1,26 +1,49 @@
-import { News } from '@/payload-types'
+import { Media, News, NewsType } from '@/payload-types'
 import Image from 'next/image'
+import convertToDate from '@/src/app/(frontend)/_utils/convertToDate'
+import ExternalLink from '../../../ExternalLink'
+import PartnerLogos from '../../../PartnerLogos'
+import Pill from '../../../Pill'
 import styles from './Slide.module.scss'
 
 export default function Slide({
-  title, slug, teaserThumbnail, publishDate,
+  title, logos, slug, teaserThumbnail, publishDate: timestamp, type,
 }: News) {
   return (
-    <div className={styles.slide}>
-      {typeof teaserThumbnail === 'object' && teaserThumbnail?.url && (
-        <div className={styles.imageWrap}>
-          <Image
-            src={teaserThumbnail.url}
-            alt={teaserThumbnail.alt}
-            width={teaserThumbnail.width ?? 0}
-            height={teaserThumbnail.height ?? 0}
-          />
+    <div className={styles.slideWrap}>
+      <div className={styles.link}>
+        <ExternalLink
+          href={`/news/${slug}`}
+        >
+          Read more
+        </ExternalLink>
+      </div>
+      <div className={styles.slide}>
+        {typeof teaserThumbnail === 'object' && teaserThumbnail?.url && (
+          <div className={styles.imageWrap}>
+            <Image
+              src={teaserThumbnail.url}
+              alt={teaserThumbnail.alt}
+              width={teaserThumbnail.width ?? 0}
+              height={teaserThumbnail.height ?? 0}
+            />
+          </div>
+        )}
+        <div className={styles.content}>
+          <h2 className={styles.eyebrow}>Flare Featured News</h2>
+          <div className={styles.content}>
+            {logos && <PartnerLogos logos={logos.map(({ image }) => image as Media)} />}
+          </div>
+          <h3 className={styles.title}>
+            {title}
+          </h3>
+          {(type || timestamp) && (
+            <div className={styles.meta}>
+              {type && <Pill text={(type as NewsType).title} size='small' />}
+              {timestamp && <time className={styles.date} dateTime={timestamp}>{convertToDate(timestamp)}</time>}
+            </div>
+          )}
         </div>
-      )}
-      <div className={styles.content}>
-        {title}
-        {slug}
-        {publishDate}
       </div>
     </div>
   )
