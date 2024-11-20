48b59b94c23b924bf7b8b3e4e1b281985f77d49a
Conrad Seto <conrad@alephsf.com>
2024-11-19 23:53:00 -0800

    feat: Add other platforms to iconMap. Remove override icon condition.

--- src/app/(frontend)/_components/OfficialChannelsIcon/OfficialChannelsIcon.tsx
+++ src/app/(frontend)/_components/OfficialChannelsIcon/OfficialChannelsIcon.tsx
@@ -1,31 +1,39 @@
 import type { SocialLink as ISocialLink } from '@/payload-types'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Discord from '../svgs/Discord'
+import GitHub from '../svgs/GitHub'
+import LinkedIn from '../svgs/LinkedIn'
+import Medium from '../svgs/Medium'
 import Telegram from '../svgs/Telegram'
 import XSocialSymbol from '../svgs/XSocialSymbol'
 import YouTube from '../svgs/YouTube'
 
 export type OfficialChannelsIconProps = {
   channelTitle?: ISocialLink['title'],
-  icon?: ISocialLink['icon']
+  icon?: ISocialLink['icon'] // to remove
 }
 
 const iconMap = {
-  Discord,
-  Telegram,
-  X: XSocialSymbol,
-  YouTube,
+  discord: Discord,
+  github: GitHub,
+  linkedin: LinkedIn,
+  medium: Medium,
+  telegram: Telegram,
+  x: XSocialSymbol,
+  youtube: YouTube,
 }
 
 export default function OfficialChannelsIcon({ channelTitle, icon }: OfficialChannelsIconProps) {
   const Icon = iconMap[channelTitle as keyof typeof iconMap]
+  console.log(icon)
 
   let cardIcon
 
-  if (icon && typeof icon !== 'number') {
-    cardIcon = icon.url
-      && <Image src={icon.url} alt={icon.alt} width={icon.width ?? 0} height={icon.height ?? 0} />
-  } else if (Icon) {
+  // if (icon && typeof icon !== 'number') {
+  //   cardIcon = icon.url
+  //     && <Image src={icon.url} alt={icon.alt} width={icon.width ?? 0} height={icon.height ?? 0} />
+  // } else
+  if (Icon) {
     cardIcon = <Icon />
   } else {
     cardIcon = null
