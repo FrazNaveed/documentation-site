a259e173fb44348a59dc3c403cd7984769becdf0
Conrad Seto <conrad@alephsf.com>
2024-11-21 11:09:23 -0800

    feat: Update component to use selected social channels from global social channels

--- src/app/(frontend)/_components/OfficialChannelsBlock/OfficialChannelsBlock.tsx
+++ src/app/(frontend)/_components/OfficialChannelsBlock/OfficialChannelsBlock.tsx
@@ -1,6 +1,7 @@
 import cx from 'classnames'
 import type { OfficialChannelsBlock as OCBlock } from '@/payload-types'
 import type { PayloadLexicalReactRendererContent } from '../LexicalRenderer/LexicalRenderer'
+import { getGlobalSocialChannels } from '../../_lib/payload/pageQueries'
 import OfficialChannelsIcon from '../OfficialChannelsIcon'
 import ExternalLink from '../ExternalLink'
 import LexicalRenderer from '../LexicalRenderer'
@@ -9,13 +10,47 @@ import styles from './OfficialChannelsBlock.module.scss'
 export type OfficialChannelsBlockProps = {
   title?: OCBlock['title'],
   text?: OCBlock['text'],
-  channels?: OCBlock['channels'],
+  selectSocialChannels?: string[] | null,
+  lang: 'en' | 'es' | 'de' | undefined,
   className?: string,
 }
 
-export default function OfficialChannelsBlock({
-  title, text, channels, className,
+interface ISocialChannel {
+  title: string;
+  url: string;
+  followerCount: number | null;
+}
+
+interface IGlobalSocialChannels {
+  [key: string]: ISocialChannel;
+}
+
+function filterAndOrderSocialChannels(
+  globalSocialChannels: IGlobalSocialChannels,
+  selectSocialChannels: OfficialChannelsBlockProps['selectSocialChannels'] | null,
+): Array<ISocialChannel & { key: string }> {
+  if (!selectSocialChannels) return []
+
+  return selectSocialChannels
+    .map((key) => {
+      const channel = globalSocialChannels[key]
+      if (channel) {
+        return {
+          key,
+          ...channel,
+        }
+      }
+      return null
+    })
+    .filter((channel): channel is ISocialChannel & { key: string } => channel !== null)
+}
+
+export default async function OfficialChannelsBlock({
+  title, text, lang, selectSocialChannels, className,
 }: OfficialChannelsBlockProps) {
+  const globalSocialChannels: IGlobalSocialChannels = await getGlobalSocialChannels(lang)
+  const socialMediaChannels = filterAndOrderSocialChannels(globalSocialChannels, selectSocialChannels)
+
   return (
     <section className={cx(styles.wrap, className)}>
       {title && <h2 className={styles.title}>{title}</h2>}
@@ -26,22 +61,21 @@ export default function OfficialChannelsBlock({
           </div>
         )}
       <div className={styles.channels} role='list'>
-        {channels?.map((channel) => {
-          if (typeof channel === 'number') return null
+        {socialMediaChannels?.map((socialMediaChannel) => {
+          if (typeof socialMediaChannel === 'number') return null
 
           const {
+            key,
             title: channelTitle,
             url,
-            icon,
-            id,
-          } = channel
+          } = socialMediaChannel
 
-          const cardIcon = <OfficialChannelsIcon channelTitle={channelTitle} icon={icon} />
-
+          const cardIcon = <OfficialChannelsIcon channelTitle={key} />
+          console.log(cardIcon)
           return (
             <ExternalLink
               href={url}
-              key={id}
+              key={key}
               className={styles.channel}
               aria-label={`Visit our ${channelTitle} channel`}
             >
