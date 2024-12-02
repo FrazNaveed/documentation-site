5f7e7a2ed98aaf2c4743a99c9a22c98708adcf65
Conrad Seto <conrad@alephsf.com>
2024-12-02 14:46:56 -0800

    feat: Add code snippets for GTM

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import SiteHeader from './_components/SiteHeader'
 import SiteFooter from './_components/SiteFooter'
 import satoshi from './_lib/fonts'
+import GoogleTagManager, { GoogleTagManagerNoscript } from './_components/GoogleTagManager/GoogleTagManager'
 import './_styles/globals.scss'
 
 export default function RootLayout({
@@ -10,7 +11,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <GoogleTagManager gtmId='GTM-WX2D2TR' />
+      </head>
       <body className={satoshi.variable}>
+        <GoogleTagManagerNoscript gtmId='GTM-WX2D2TR' />
         <a className='skipToMainContentLink' href='#mainContent'>
           Skip to content
         </a>
