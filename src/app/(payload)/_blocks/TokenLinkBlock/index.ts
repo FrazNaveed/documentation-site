1691ef78a2fcb895c187f7b7c2941780026cffc5
Conrad Seto <conrad@alephsf.com>
2024-12-02 16:11:31 -0800

    feat: Create Token Link Block payload

---
+++ src/app/(payload)/_blocks/TokenLinkBlock/index.ts
@@ -0,0 +1,39 @@
+import { i18n } from '@/src/app/i18n-config'
+import { BlockMarginSettings } from '../../_fields/BlockMarginSettings'
+import { CreateSideNavLinkFields } from '../../_fields/CreateSideNavLink'
+import { Block } from 'payload'
+import { array } from 'payload/shared'
+import validateTextFieldUrl from '../../_utils/validateTextFieldUrl'
+
+export const TokenLinkBlock: Block = {
+  slug: 'token-link',
+  interfaceName: 'ITokenLinkBlock',
+  dbName: 'tokenLink',
+  fields: [
+    {
+      name: 'tokenLinks',
+      type: 'array',
+      localized: true,
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'link',
+          type: 'text',
+          validate: validateTextFieldUrl,
+        },
+        {
+          name: 'icon',
+          type: 'relationship',
+          relationTo: 'media',
+        },
+      ],
+    },
+    ...CreateSideNavLinkFields,
+    ...BlockMarginSettings,
+  ],
+  imageURL: `/${i18n.defaultLocale}/icons/link.svg`,
+  imageAltText: 'Token Link block icon',
+}
