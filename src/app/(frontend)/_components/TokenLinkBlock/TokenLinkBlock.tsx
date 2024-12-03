1c0fcc0bef474018f2aae44ff168b377b475e5fb
Conrad Seto <conrad@alephsf.com>
2024-12-03 11:11:21 -0800

    feat: Create component

--- src/app/(frontend)/_components/TokenLinkBlock/TokenLinkBlock.tsx
+++ src/app/(frontend)/_components/TokenLinkBlock/TokenLinkBlock.tsx
@@ -1,7 +1,51 @@
-export default function TokenLinkBlock() {
+import cx from 'classnames'
+import { ITokenLinkBlock } from '@/payload-types'
+import Image from 'next/image'
+import Link from '../ExternalLink'
+import styles from './TokenLinkBlock.module.scss'
+
+export type TokenLinkBlockProps = ITokenLinkBlock & {
+  className?: string
+}
+
+export default function TokenLinkBlock({
+  tokenLinks,
+  className,
+}: TokenLinkBlockProps) {
   return (
-    <section>
-      hi
+    <section className={cx(styles.wrap, className)}>
+      <div className={styles.linkWrap}>
+        {tokenLinks?.map((link) => {
+          const {
+            title,
+            link: url,
+            icon,
+            id,
+          } = link
+          return (
+            url && (
+            <Link
+              key={id}
+              href={url}
+              className={styles.link}
+            >
+              {icon && typeof icon === 'object' && icon.url && (
+                <Image
+                  src={icon.url}
+                  alt={icon.alt}
+                  width={icon.width ?? 0}
+                  height={icon.height ?? 0}
+                  className={styles.linkIcon}
+                />
+              )}
+              <div className={styles.linkText}>
+                {title}
+              </div>
+            </Link>
+            )
+          )
+        })}
+      </div>
     </section>
   )
 }
