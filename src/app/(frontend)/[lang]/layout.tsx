16c8ab5f82baf850b0004fcc59accf8468ca1839
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-12-03 13:53:45 -0800

    feat: Add lang to HTML

--- src/app/(frontend)/[lang]/layout.tsx
+++ src/app/(frontend)/[lang]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Viewport } from 'next'
+import type { Locale } from 'src/app/i18n-config'
 import SiteHeader from '../_components/SiteHeader'
 import SiteFooter from '../_components/SiteFooter'
 import satoshi from '../_lib/fonts'
@@ -6,6 +7,13 @@ import { defaultMetadata } from '../_utils/buildMetadata'
 import GoogleTagManager, { GoogleTagManagerNoscript } from '../_components/GoogleTagManager/GoogleTagManager'
 import '../_styles/globals.scss'
 import CookieYes from '../_components/CookieYes/CookieYes'
+
+type LayoutProps = {
+  children: React.ReactNode
+  params: {
+    lang: Locale
+  }
+}
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -15,13 +23,10 @@ export const viewport: Viewport = {
 
 export const metadata = defaultMetadata
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children, params }: LayoutProps) {
+  const { lang } = params
   return (
-    <html lang='en'>
+    <html lang={lang}>
       <head>
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
         <CookieYes />
