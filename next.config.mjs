def14746357484b847bbc2f60b639644b43c563a
Matt Glaser <matt@alephsf.com>
2024-12-03 18:21:10 -0800

    chore(Cache): Add sensible cache-control headers

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,26 @@ const nextConfig = {
   sassOptions: {
     silenceDeprecations: ['mixed-decls', 'legacy-js-api'],
   },
+  headers: () => [
+    {
+      source: '/:path*',
+      headers: [
+        { key: 'Cache-Control', value: 'public, max-age=600, stale-while-revalidate=60' },
+      ],
+    },
+    {
+      source: '/api/(.*)',
+      headers: [
+        { key: 'Cache-Control', value: 'private, no-cache, no-store, max-age=0, must-revalidate' },
+      ],
+    },
+    {
+      source: '/_next/static/(.*)',
+      headers: [
+        { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+      ],
+    },
+  ],
 }
 
 export default bundleAnalyzer(withPayload(nextConfig))
