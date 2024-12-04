b20cdc60eed1ae03b9156713b62349bf6dad0c18
Conrad Seto <conrad@alephsf.com>
2024-12-04 11:51:19 -0800

    feat: Add wrap, move class

--- src/app/(frontend)/_components/BannerCTA/BannerCTA.tsx
+++ src/app/(frontend)/_components/BannerCTA/BannerCTA.tsx
@@ -14,16 +14,18 @@ export default function BannerCTA({
   header, text, button, className,
 }: CTAProps) {
   return (
-    <section className={cx(styles.banner, className)}>
-      <h2 className={styles.header}>{header}</h2>
-      <div className={styles.rightColumn}>
-        {text && <p className={styles.text}>{text}</p>}
-        <Button
-          text={button.text}
-          link={button.link}
-          className={styles.button}
-          buttonStyle='secondary'
-        />
+    <section className={styles.wrap}>
+      <div className={cx(styles.banner, className)}>
+        <h2 className={styles.header}>{header}</h2>
+        <div className={styles.rightColumn}>
+          {text && <p className={styles.text}>{text}</p>}
+          <Button
+            text={button.text}
+            link={button.link}
+            className={styles.button}
+            buttonStyle='secondary'
+          />
+        </div>
       </div>
     </section>
   )
