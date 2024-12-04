03dc6ac286f07e0df66db4638b4e54559fc67b27
Matt Glaser <matt@alephsf.com>
2024-12-03 17:32:57 -0800

    feat(Cache): Add NextJS unstable_cache to local api connection

--- src/app/(frontend)/_lib/payload/devGuideQueries.ts
+++ src/app/(frontend)/_lib/payload/devGuideQueries.ts
@@ -1,23 +1,32 @@
+// eslint-disable-next-line camelcase
+import { unstable_cache } from 'next/cache'
 import { getPayload } from 'payload'
 import config from '@payload-config'
 
 const payload = await getPayload({ config })
 
 // eslint-disable-next-line import/prefer-default-export
-export const getDevGuides = async (
-  limit = 100,
-  page = 1,
-) => {
-  try {
-    const devGuideData = await payload.find({
-      collection: 'developerGuides',
-      limit,
-      page,
-      sort: '-publishDate',
-    })
-    return devGuideData
-  } catch (error) {
-    console.error('Error fetching getDevGuides:', error)
-  }
-  return null
-}
+export const getDevGuides = unstable_cache(
+  async (
+    limit = 100,
+    page = 1,
+  ) => {
+    try {
+      const devGuideData = await payload.find({
+        collection: 'developerGuides',
+        limit,
+        page,
+        sort: '-publishDate',
+      })
+      return devGuideData
+    } catch (error) {
+      console.error('Error fetching getDevGuides:', error)
+    }
+    return null
+  },
+  [],
+  {
+    tags: ['developerGuides'],
+    revalidate: 60,
+  },
+)
