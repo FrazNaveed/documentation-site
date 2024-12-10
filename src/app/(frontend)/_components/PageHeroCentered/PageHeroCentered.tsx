d7418572122c2257d0ac68f8e6c51a8726cd7d1e
Matt Glaser <matt@alephsf.com>
2024-12-10 14:01:26 -0800

    chore: Change mimetype on video to quicktime

--- src/app/(frontend)/_components/PageHeroCentered/PageHeroCentered.tsx
+++ src/app/(frontend)/_components/PageHeroCentered/PageHeroCentered.tsx
@@ -163,7 +163,7 @@ export default function PageHeroCentered({
   const videoMarkup = (modiferClas?: string) => (showBackgroundVideo ? (
     <div className={cx(styles.videoWrap, styles[`videoWrap__${modiferClas}`])}>
       <video className={cx(styles.video, styles[`video__${modiferClas}`])} autoPlay loop muted>
-        <source src='/en/video/home_hero.mov' type='video/mp4; codecs="hvc1"' />
+        <source src='/en/video/home_hero.mov' type='video/quicktime' />
         <source src='/en/video/home_hero.webm' type='video/webm' />
       </video>
     </div>
