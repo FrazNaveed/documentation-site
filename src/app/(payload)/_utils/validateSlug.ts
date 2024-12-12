60c5e56f0bac0e6ce6635f0db3e82258c1cf1dad
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-12-12 14:52:26 -0800

    feat: Specify only lowercase letters allowed in slugs

--- src/app/(payload)/_utils/validateSlug.ts
+++ src/app/(payload)/_utils/validateSlug.ts
@@ -8,7 +8,7 @@ const validateSlug = (value: string | null | undefined) => {
   const disallowedSpecialChars = /[^a-z0-9-]/g // Only allow alphanumeric characters and hyphens
   const disAllowedMatches = value?.match(disallowedSpecialChars)
   if (value && disAllowedMatches !== null) {
-    return 'Slug must contain only letters (a-z), numbers (0-9), and hyphens (-). Special characters and spaces are not allowed.'
+    return 'Slug must contain only lowercase letters (a-z), numbers (0-9), and hyphens (-).'
   }
 
   return true
