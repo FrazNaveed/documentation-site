7ff9450d98772122b7da1d6951dbb5722025389b
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-12-12 10:00:55 -0800

    feat: Add playsinline to Page Hero video

--- src/app/(frontend)/_components/PageHeroCentered/PageHeroCentered.tsx
+++ src/app/(frontend)/_components/PageHeroCentered/PageHeroCentered.tsx
@@ -162,7 +162,7 @@ export default function PageHeroCentered({
   )
   const videoMarkup = (modiferClas?: string) => (showBackgroundVideo ? (
     <div className={cx(styles.videoWrap, styles[`videoWrap__${modiferClas}`])}>
-      <video className={cx(styles.video, styles[`video__${modiferClas}`])} autoPlay loop muted>
+      <video className={cx(styles.video, styles[`video__${modiferClas}`])} autoPlay loop muted playsInline>
         <source src='/en/video/home_hero.mov' type='video/quicktime' />
         <source src='/en/video/home_hero.webm' type='video/webm' />
       </video>
