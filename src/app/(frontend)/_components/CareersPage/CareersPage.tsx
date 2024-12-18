379a24def530a2159ff622aebc19b1600aef5b52
Conrad Seto <conrad@alephsf.com>
2024-12-18 15:33:07 -0800

    feat: Add teaser grid to landing page

--- src/app/(frontend)/_components/CareersPage/CareersPage.tsx
+++ src/app/(frontend)/_components/CareersPage/CareersPage.tsx
@@ -1,11 +1,15 @@
 import getCareersListings from '../../_lib/payload/careersQueries'
+import CareersTeaserGrid from './CareersTeaserGrid'
+import styles from './CareersPage.module.scss'
 
 export default async function CareersPage() {
   const careers = await getCareersListings()
   console.log(careers)
   return (
-    <div>
-      Careers Landing Page Component
+    <div className={styles.wrap}>
+      {/* get copy from page template */}
+      <h2>Open Roles</h2>
+      <CareersTeaserGrid careers={careers} />
     </div>
   )
 }
