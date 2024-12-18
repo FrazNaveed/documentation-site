9e860437c3efaf48a3b17c4bf13ff354590c0a88
Conrad Seto <conrad@alephsf.com>
2024-12-18 14:51:01 -0800

    feat: Add query to Careers landing page

--- src/app/(frontend)/_components/CareersPage/CareersPage.tsx
+++ src/app/(frontend)/_components/CareersPage/CareersPage.tsx
@@ -1,4 +1,8 @@
+import getCareersListings from '../../_lib/payload/careersQueries'
+
 export default async function CareersPage() {
+  const careers = await getCareersListings()
+  console.log(careers)
   return (
     <div>
       Careers Landing Page Component
