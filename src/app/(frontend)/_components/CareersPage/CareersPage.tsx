800604dfca35a645daea33ec5a4e24715c88efc9
Conrad Seto <conrad@alephsf.com>
2024-12-18 16:41:48 -0800

    feat: Add props from page template to component. To-do: Hook up rendering for Lexical

--- src/app/(frontend)/_components/CareersPage/CareersPage.tsx
+++ src/app/(frontend)/_components/CareersPage/CareersPage.tsx
@@ -1,15 +1,27 @@
 import getCareersListings from '../../_lib/payload/careersQueries'
+import { getPageBySlug } from '../../_lib/payload/pageQueries'
 import CareersTeaserGrid from './CareersTeaserGrid'
 import styles from './CareersPage.module.scss'
 
 export default async function CareersPage() {
-  const careers = await getCareersListings()
-  console.log(careers)
+  const careersListings = await getCareersListings()
+  const careersPage = await getPageBySlug('careers')
+  const careersPageData = careersPage[0] || {}
+  // console.log(careers)
+  // console.log('page: ', careersPageData)
+  const {
+    careers: {
+      pageTitle,
+      // content,
+      emptyListingsText,
+    } = {},
+  } = careersPageData
+
   return (
     <div className={styles.wrap}>
       {/* get copy from page template */}
-      <h2>Open Roles</h2>
-      <CareersTeaserGrid careers={careers} />
+      <h2>{pageTitle || 'Open Roles'}</h2>
+      <CareersTeaserGrid careers={careersListings} emptyListingsText={emptyListingsText} />
     </div>
   )
 }
