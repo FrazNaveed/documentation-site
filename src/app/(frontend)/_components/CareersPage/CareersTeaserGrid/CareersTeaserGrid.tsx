ef7ecc69cffe541987343c7b3c51c361dcd3bb66
Conrad Seto <conrad@alephsf.com>
2024-12-19 00:45:51 -0800

    feat: Create teaser grid component

--- src/app/(frontend)/_components/CareersPage/CareersTeaserGrid/CareersTeaserGrid.tsx
+++ src/app/(frontend)/_components/CareersPage/CareersTeaserGrid/CareersTeaserGrid.tsx
@@ -1,19 +1,56 @@
 import LexicalRenderer from '../../LexicalRenderer'
+import Link from '../../Link'
+import RightArrow from '../../svgs/RightArrow'
+import getCollectionPath from '../../../_utils/getCollectionPath'
+import styles from './CareersTeaserGrid.module.scss'
 
 type CareersTeaserGridProps = {
-  careers: any
+  careersListings: any
   emptyListingsText: any
 }
 
-export default function CareersTeaserGrid({ careers, emptyListingsText }: CareersTeaserGridProps) {
-  if (careers.docs.length === 0) {
+export default function CareersTeaserGrid({ careersListings, emptyListingsText }: CareersTeaserGridProps) {
+  const { docs } = careersListings
+  console.log('hello', docs)
+  if (docs.length === 0) {
     return (
-      <LexicalRenderer content={emptyListingsText} />
+      <div>
+        <LexicalRenderer content={emptyListingsText} />
+      </div>
     )
   }
   return (
-    <div>
-      GRID!!!!!!!!
+    <div className={styles.careerTeaserGridWrap}>
+      {docs && docs.map((career: any) => {
+        const {
+          id,
+          slug,
+          productTeam: { teamName },
+          jobTitle,
+          description,
+          locations,
+        } = career
+        const link = `${getCollectionPath('careers')}${slug}`
+        return (
+          <Link key={id} href={link} className={styles.careersTeaser}>
+            <div>
+              {teamName}
+              <RightArrow />
+            </div>
+            {jobTitle}
+            {description && <LexicalRenderer content={description} />}
+            {Object.entries(locations)
+              .filter(([, value]) => value === true)
+              .map(([key], index, array) => (
+                <span
+                  key={key}
+                >
+                  {`${key}${index === array.length - 1 ? '' : ' / '}`}
+                </span>
+              ))}
+          </Link>
+        )
+      })}
     </div>
   )
 }
