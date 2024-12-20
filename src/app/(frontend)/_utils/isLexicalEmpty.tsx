34c6eccb891855d337773364afdf852e5b1be499
Amanda Tusing <1373926+amanda-t@users.noreply.github.com>
2024-12-20 14:45:50 -0800

    feat: Check for null/undefined in isLexicalEmpty
    And use it in MarqueeGallery

--- src/app/(frontend)/_utils/isLexicalEmpty.tsx
+++ src/app/(frontend)/_utils/isLexicalEmpty.tsx
@@ -1,8 +1,23 @@
-// Using any as we're only checking the structure
-export default function isLexicalEmpty(content: any): boolean {
+import type { SerializedEditorState, SerializedLexicalNode } from '@payloadcms/richtext-lexical/lexical'
+
+interface SerializedParagraphNode extends SerializedLexicalNode {
+  type: 'paragraph';
+  children: SerializedLexicalNode[]
+}
+
+type TContent = SerializedEditorState | null | undefined
+
+// If content had been entered and deleted,
+// Lexical field will no longer be null, but rather a root element with an empty paragraph child
+// Check for this so to determine if there is Lexical content or not
+export default function isLexicalEmpty(content: TContent): boolean {
+  if (!content) {
+    return true
+  }
+  const contentRootChildren = content?.root?.children
   return (
-    content?.root?.children?.[0]?.type === 'paragraph'
-      && content?.root?.children?.[0]?.children?.length === 0
-      && content?.root?.children?.length === 1
+    contentRootChildren?.length === 1
+      && contentRootChildren?.[0]?.type === 'paragraph'
+      && (contentRootChildren?.[0] as SerializedParagraphNode)?.children.length === 0
   )
 }
